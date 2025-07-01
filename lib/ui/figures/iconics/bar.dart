ff9d858dd46c2a9abd50ac61feb4fe8038845423
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-01 16:14:12 +0300

    add abstract bar class and fix parameters

--- lib/ui/figures/iconics/bar.dart
+++ lib/ui/figures/iconics/bar.dart
@@ -5,13 +5,17 @@ import 'package:studio/ui/figures/iconic.dart';
 const k_bar_length = 0.6;
 const k_bar_gap = 0.4;
 
-class BarIconic extends Iconic {
+abstract class BarIconic extends Iconic {
   final double barGap;
   final double barLength;
 
   BarIconic({required super.size, required super.iconicStyle})
     : barGap = size * k_bar_gap,
       barLength = size * k_bar_length;
+}
+
+class HorizontalBarIconic extends BarIconic {
+  HorizontalBarIconic({required super.size, required super.iconicStyle});
 
   @override
   void paint(Canvas canvas, Offset offset) {
@@ -33,28 +37,24 @@ class BarIconic extends Iconic {
   }
 }
 
-class VerticalBarIconic extends Iconic {
-  final double _size_gap;
-  final double _size_length;
-  VerticalBarIconic({required super.size, required super.iconicStyle})
-    : _size_gap = size * k_bar_gap,
-      _size_length = size * k_bar_length;
+class VerticalBarIconic extends BarIconic {
+  VerticalBarIconic({required super.size, required super.iconicStyle});
 
   @override
   void paint(Canvas canvas, Offset offset) {
     canvas.drawLine(
-      Offset(offset.dx - _size_gap, offset.dy - _size_length),
-      Offset(offset.dx - _size_gap, offset.dy + _size_length),
+      Offset(offset.dx - barGap, offset.dy - barLength),
+      Offset(offset.dx - barGap, offset.dy + barLength),
       strokeStyle,
     );
     canvas.drawLine(
-      Offset(offset.dx, offset.dy - _size_length),
-      Offset(offset.dx, offset.dy + _size_length),
+      Offset(offset.dx, offset.dy - barLength),
+      Offset(offset.dx, offset.dy + barLength),
       strokeStyle,
     );
     canvas.drawLine(
-      Offset(offset.dx + _size_gap, offset.dy - _size_length),
-      Offset(offset.dx + _size_gap, offset.dy + _size_length),
+      Offset(offset.dx + barGap, offset.dy - barLength),
+      Offset(offset.dx + barGap, offset.dy + barLength),
       strokeStyle,
     );
   }
