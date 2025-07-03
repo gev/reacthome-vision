66cf91469c85b43939245034abd6ddb6f2616d08
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-03 16:21:56 +0300

    device icon

--- lib/ui/figures/iconics/device.dart
+++ lib/ui/figures/iconics/device.dart
@@ -3,23 +3,31 @@ import 'dart:ui';
 import 'package:studio/ui/figures/iconic.dart';
 
 const k_device_size = 0.5;
-const k_circle_size = 0.35;
+const k_circle_size = 0.6;
 
 class DeviceIconic extends Iconic {
+  final Offset offset;
   final double device_size;
   final double circle_size;
-  DeviceIconic({required super.size, required super.iconicStyle})
-    : device_size = size * k_device_size,
-      circle_size = size * k_circle_size;
+  final Path _path = Path();
+  DeviceIconic({
+    required this.offset,
+    required super.size,
+    required super.iconicStyle,
+  }) : device_size = size * k_device_size,
+       circle_size = size * k_circle_size {
+    _path
+      ..addRect(Rect.fromCenter(center: offset, width: size, height: size))
+      ..addOval(
+        Rect.fromCenter(
+          center: offset,
+          width: circle_size,
+          height: circle_size,
+        ),
+      );
+  }
   @override
   void paint(Canvas canvas) {
-    // canvas.drawRect(
-    //   Rect.fromPoints(
-    //     Offset(offset.dx - device_size, offset.dy + device_size),
-    //     Offset(offset.dx + device_size, offset.dy - device_size),
-    //   ),
-    //   strokeStyle,
-    // );
-    // canvas.drawCircle(Offset(offset.dx, offset.dy), circle_size, strokeStyle);
+    canvas.drawPath(_path, strokeStyle);
   }
 }
