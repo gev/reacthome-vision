60655ade27d985072a9c3583da5c3c0e1024e94a
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-03 16:21:24 +0300

    bar icon

--- lib/ui/figures/iconics/bar.dart
+++ lib/ui/figures/iconics/bar.dart
@@ -2,60 +2,56 @@ import 'dart:ui';
 
 import 'package:studio/ui/figures/iconic.dart';
 
-const k_bar_length = 0.6;
-const k_bar_gap = 0.4;
+const k_bar_length = 0.5;
+const k_bar_gap = 0.35;
 
 abstract class BarIconic extends Iconic {
+  final Offset offset;
   final double barGap;
   final double barLength;
+  final Path _path = Path();
 
-  BarIconic({required super.size, required super.iconicStyle})
-    : barGap = size * k_bar_gap,
-      barLength = size * k_bar_length;
-}
-
-class HorizontalBarIconic extends BarIconic {
-  HorizontalBarIconic({required super.size, required super.iconicStyle});
+  BarIconic({
+    required this.offset,
+    required super.size,
+    required super.iconicStyle,
+  }) : barGap = size * k_bar_gap,
+       barLength = size * k_bar_length;
 
   @override
   void paint(Canvas canvas) {
-    // canvas.drawLine(
-    //   Offset(offset.dx - barLength, offset.dy),
-    //   Offset(offset.dx + barLength, offset.dy),
-    //   strokeStyle,
-    // );
-    // canvas.drawLine(
-    //   Offset(offset.dx - barLength, offset.dy - barGap),
-    //   Offset(offset.dx + barLength, offset.dy - barGap),
-    //   strokeStyle,
-    // );
-    // canvas.drawLine(
-    //   Offset(offset.dx - barLength, offset.dy + barGap),
-    //   Offset(offset.dx + barLength, offset.dy + barGap),
-    //   strokeStyle,
-    // );
+    canvas.drawPath(_path, strokeStyle);
+  }
+}
+
+class HorizontalBarIconic extends BarIconic {
+  HorizontalBarIconic({
+    required super.offset,
+    required super.size,
+    required super.iconicStyle,
+  }) {
+    _path
+      ..moveTo(offset.dx - barLength, offset.dy - barGap)
+      ..relativeLineTo(size, 0)
+      ..relativeMoveTo(-size, barGap)
+      ..relativeLineTo(size, 0)
+      ..relativeMoveTo(-size, barGap)
+      ..relativeLineTo(size, 0);
   }
 }
 
 class VerticalBarIconic extends BarIconic {
-  VerticalBarIconic({required super.size, required super.iconicStyle});
-
-  @override
-  void paint(Canvas canvas) {
-    // canvas.drawLine(
-    //   Offset(offset.dx - barGap, offset.dy - barLength),
-    //   Offset(offset.dx - barGap, offset.dy + barLength),
-    //   strokeStyle,
-    // );
-    // canvas.drawLine(
-    //   Offset(offset.dx, offset.dy - barLength),
-    //   Offset(offset.dx, offset.dy + barLength),
-    //   strokeStyle,
-    // );
-    // canvas.drawLine(
-    //   Offset(offset.dx + barGap, offset.dy - barLength),
-    //   Offset(offset.dx + barGap, offset.dy + barLength),
-    //   strokeStyle,
-    // );
+  VerticalBarIconic({
+    required super.offset,
+    required super.size,
+    required super.iconicStyle,
+  }) {
+    _path
+      ..moveTo(offset.dx - barGap, offset.dy - barLength)
+      ..relativeLineTo(0, size)
+      ..relativeMoveTo(barGap, -size)
+      ..relativeLineTo(0, size)
+      ..relativeMoveTo(barGap, -size)
+      ..relativeLineTo(0, size);
   }
 }
