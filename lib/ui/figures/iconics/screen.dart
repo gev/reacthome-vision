cdb41d784ceff4c399a687b8df57d52f87f286b6
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-06 13:32:42 +0300

    screen path

--- lib/ui/figures/iconics/screen.dart
+++ lib/ui/figures/iconics/screen.dart
@@ -7,29 +7,26 @@ const k_screen_height = 0.5;
 const k_screen_top = 0.35;
 
 class ScreenIconic extends Iconic {
+  final Path _path = Path();
   final double screenWidth;
   final double screenHeight;
   final double screenTop;
-  ScreenIconic({
-    required super.offset,
-    required super.size,
-    required super.iconicStyle,
-  }) : screenWidth = size * k_screen_width,
-       screenHeight = size * k_screen_height,
-       screenTop = size * k_screen_top;
+  ScreenIconic({required super.offset, required super.size, required super.iconicStyle})
+    : screenWidth = size * k_screen_width,
+      screenHeight = size * k_screen_height,
+      screenTop = size * k_screen_top {
+    _path
+      ..moveTo(offset.dx - screenWidth, offset.dy - screenTop)
+      ..relativeLineTo(2 * screenWidth, 0)
+      ..addRect(
+        Rect.fromPoints(
+          Offset(offset.dx - screenWidth, offset.dy + screenHeight),
+          Offset(offset.dx + screenWidth, offset.dy - screenHeight),
+        ),
+      );
+  }
   @override
   void paint(Canvas canvas) {
-    // canvas.drawRect(
-    //   Rect.fromPoints(
-    //     Offset(offset.dx - screenWidth, offset.dy + screenHeight),
-    //     Offset(offset.dx + screenWidth, offset.dy - screenHeight),
-    //   ),
-    //   strokeStyle,
-    // );
-    // canvas.drawLine(
-    //   Offset(offset.dx - screenWidth, offset.dy - screenTop),
-    //   Offset(offset.dx + screenWidth, offset.dy - screenTop),
-    //   strokeStyle,
-    // );
+    canvas.drawPath(_path, strokeStyle);
   }
 }
