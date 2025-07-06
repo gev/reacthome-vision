091a1d2283f4d0c39f3fc6b3a2a17a706b13d725
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-06 13:31:57 +0300

    curtains path

--- lib/ui/figures/iconics/curtains.dart
+++ lib/ui/figures/iconics/curtains.dart
@@ -2,43 +2,40 @@ import 'dart:ui';
 
 import 'package:studio/ui/figures/iconic.dart';
 
-const k_curtains_width = 0.6;
-const k_curtains_sides = 0.4;
-const k_curtains_height = 0.6;
+const k_curtains_width = 0.5;
+const k_curtains_sides = 0.3;
+const k_curtains_height = 0.5;
 
 class CurtainsIconic extends Iconic {
   final double curtainsSides;
   final double curtainsHeight;
   final double curtainsWidth;
+  final Path _path = Path();
 
-  CurtainsIconic({
-    required super.offset,
-    required super.size,
-    required super.iconicStyle,
-  }) : curtainsSides = size * k_curtains_sides,
-       curtainsHeight = size * k_curtains_height,
-       curtainsWidth = size * k_curtains_width;
+  CurtainsIconic({required super.offset, required super.size, required super.iconicStyle})
+    : curtainsSides = size * k_curtains_sides,
+      curtainsHeight = size * k_curtains_height,
+      curtainsWidth = size * k_curtains_width {
+    _path
+      ..moveTo(offset.dx - curtainsWidth, offset.dy - curtainsHeight)
+      ..relativeLineTo(size, 0)
+      ..relativeMoveTo(-size, 0)
+      ..addRect(
+        Rect.fromPoints(
+          Offset(offset.dx - curtainsWidth, offset.dy - curtainsHeight),
+          Offset(offset.dx - curtainsSides, offset.dy + curtainsHeight),
+        ),
+      )
+      ..addRect(
+        Rect.fromPoints(
+          Offset(offset.dx + curtainsSides, offset.dy + curtainsHeight),
+          Offset(offset.dx + curtainsWidth, offset.dy - curtainsHeight),
+        ),
+      );
+  }
 
   @override
   void paint(Canvas canvas) {
-    // canvas.drawRect(
-    //   Rect.fromPoints(
-    //     Offset(offset.dx + curtainsSides, offset.dy - curtainsHeight),
-    //     Offset(offset.dx + curtainsWidth, offset.dy + curtainsHeight),
-    //   ),
-    //   strokeStyle,
-    // );
-    // canvas.drawRect(
-    //   Rect.fromPoints(
-    //     Offset(offset.dx - curtainsWidth, offset.dy + curtainsHeight),
-    //     Offset(offset.dx - curtainsSides, offset.dy - curtainsHeight),
-    //   ),
-    //   strokeStyle,
-    // );
-    // canvas.drawLine(
-    //   Offset(offset.dx - curtainsWidth, offset.dy - curtainsHeight),
-    //   Offset(offset.dx + curtainsWidth, offset.dy - curtainsHeight),
-    //   strokeStyle,
-    // );
+    canvas.drawPath(_path, strokeStyle);
   }
 }
