2a3c52468fe2d605c67cd5c4f154972c8e55872e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-07-06 23:49:40 +0300

    fix node scheme

--- lib/ui/scheme/samples.dart
+++ lib/ui/scheme/samples.dart
@@ -8,12 +8,12 @@ final connectors = [PortType.sensor, PortType.actuator, PortType.dummy];
 final nodeScheme = Scheme(
   id: 0,
   items: [
-    Item(id: 0, x: 0, y: 0, type: ItemType.minus, connectors: connectors),
-    Item(id: 1, x: 1, y: 0, type: ItemType.plus, connectors: connectors),
-    Item(id: 2, x: 2, y: 0, type: ItemType.rightArrow, connectors: connectors),
-    Item(id: 3, x: 3, y: 0, type: ItemType.horizontalBar, connectors: connectors),
-    Item(id: 4, x: 4, y: 0, type: ItemType.verticalBar, connectors: connectors),
-    Item(id: 5, x: 0, y: 1, type: ItemType.mixer, connectors: connectors),
+    Item(id: 0, x: 3, y: 3, type: ItemType.plus, connectors: connectors),
+    Item(id: 1, x: 7, y: 3, type: ItemType.device, connectors: connectors),
+    Item(id: 2, x: 3, y: 5, type: ItemType.plus, connectors: connectors),
+    Item(id: 3, x: 7, y: 5, type: ItemType.device, connectors: connectors),
+    Item(id: 4, x: 3, y: 7, type: ItemType.plus, connectors: connectors),
+    Item(id: 5, x: 7, y: 7, type: ItemType.device, connectors: connectors),
   ],
   links: [
     Link(id: 0, source: Connector(id: 0, idx: 0), sink: Connector(id: 1, idx: 0)),
