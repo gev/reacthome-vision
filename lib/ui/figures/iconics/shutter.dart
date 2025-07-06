01814f6b70470465773eb4192f2f1d5bd4a12cd8
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-06 13:32:50 +0300

    shutter path

--- lib/ui/figures/iconics/shutter.dart
+++ lib/ui/figures/iconics/shutter.dart
@@ -2,35 +2,30 @@ import 'dart:ui';
 
 import 'package:studio/ui/figures/iconic.dart';
 
-const k_shutter_length = 0.5;
+const k_shutter_length = 0.4;
 const k_shutter_circle = 0.15;
 
 abstract class ShutterIconic extends Iconic {
   final double shutterLength;
   final double shutterCircle;
+  final Path _path = Path();
 
-  ShutterIconic({
-    required super.offset,
-    required super.size,
-    required super.iconicStyle,
-  }) : shutterLength = size * k_shutter_length,
-       shutterCircle = size * k_shutter_circle;
+  ShutterIconic({required super.offset, required super.size, required super.iconicStyle})
+    : shutterLength = size * k_shutter_length,
+      shutterCircle = size * k_shutter_circle;
+
+  @override
+  void paint(Canvas canvas) {
+    canvas.drawPath(_path, strokeStyle);
+  }
 }
 
 class VerticalShutterIconic extends ShutterIconic {
-  VerticalShutterIconic({
-    required super.offset,
-    required super.size,
-    required super.iconicStyle,
-  });
-  @override
-  void paint(Canvas canvas) {
-    // canvas.drawLine(
-    //   Offset(offset.dx - shutterLength, offset.dy - shutterLength),
-    //   Offset(offset.dx + shutterLength, offset.dy + shutterLength),
-    //   strokeStyle,
-    // );
-    // canvas.drawCircle(Offset(offset.dx, offset.dy), shutterCircle, strokeStyle);
+  VerticalShutterIconic({required super.offset, required super.size, required super.iconicStyle}) {
+    _path
+      ..moveTo(offset.dx - shutterLength, offset.dy - shutterLength)
+      ..relativeLineTo(2 * shutterLength, 2 * shutterLength)
+      ..addOval(Rect.fromCircle(center: offset, radius: shutterCircle));
   }
 }
 
@@ -39,14 +34,10 @@ class HorizontalShutterIconic extends ShutterIconic {
     required super.offset,
     required super.size,
     required super.iconicStyle,
-  });
-  @override
-  void paint(Canvas canvas) {
-    // canvas.drawLine(
-    //   Offset(offset.dx - shutterLength, offset.dy),
-    //   Offset(offset.dx + shutterLength, offset.dy),
-    //   strokeStyle,
-    // );
-    // canvas.drawCircle(Offset(offset.dx, offset.dy), shutterCircle, strokeStyle);
+  }) {
+    _path
+      ..moveTo(offset.dx - size / 2, offset.dy)
+      ..relativeLineTo(size, 0)
+      ..addOval(Rect.fromCircle(center: offset, radius: shutterCircle));
   }
 }
