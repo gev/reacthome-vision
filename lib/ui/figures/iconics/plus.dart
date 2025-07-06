c609ffb5f8fb625798d2c1492709dc7112e93971
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-06 13:32:22 +0300

    plus path

--- lib/ui/figures/iconics/plus.dart
+++ lib/ui/figures/iconics/plus.dart
@@ -6,22 +6,17 @@ const k_plus_size = 0.5;
 
 class PlusIconic extends Iconic {
   final double plusSize;
-  PlusIconic({
-    required super.offset,
-    required super.size,
-    required super.iconicStyle,
-  }) : plusSize = size * k_plus_size;
+  final Path _path = Path();
+  PlusIconic({required super.offset, required super.size, required super.iconicStyle})
+    : plusSize = size * k_plus_size {
+    _path
+      ..moveTo(offset.dx - plusSize, offset.dy)
+      ..lineTo(offset.dx + plusSize, offset.dy)
+      ..moveTo(offset.dx, offset.dy - plusSize)
+      ..lineTo(offset.dx, offset.dy + plusSize);
+  }
   @override
   void paint(Canvas canvas) {
-    // canvas.drawLine(
-    //   Offset(offset.dx - plusSize, offset.dy),
-    //   Offset(offset.dx + plusSize, offset.dy),
-    //   strokeStyle,
-    // );
-    // canvas.drawLine(
-    //   Offset(offset.dx, offset.dy - plusSize),
-    //   Offset(offset.dx, offset.dy + plusSize),
-    //   strokeStyle,
-    // );
+    canvas.drawPath(_path, strokeStyle);
   }
 }
