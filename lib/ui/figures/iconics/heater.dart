218f36c5239a80bd1c9d344829a2fbe4b8ef15e5
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-06 16:07:14 +0300

    heater path

--- lib/ui/figures/iconics/heater.dart
+++ lib/ui/figures/iconics/heater.dart
@@ -2,48 +2,56 @@ import 'dart:ui';
 
 import 'package:studio/ui/figures/iconic.dart';
 
-const k_heater_top = 0.6;
-const k_heater_middle = 0.4;
-const k_heater_bottom = 0.6;
-const k_heater_gap = 0.4;
-const k_top_arc = 0.5;
-const k_bottom_arc = 0.5;
+const kLine = 0.5;
+const kLineToArc = 0.9;
+const kMiddleLineToArc = 0.7;
+const kHeaterGap = 0.4;
+const kArc = 0.2;
 
 class HeaterIconic extends Iconic {
-  final double heaterTop;
-  final double heaterMiddle;
-  final double heaterBottom;
+  final double heaterLine;
+  final double lineToArc;
+  final double middleLineToArc;
   final double heaterGap;
+  final Path _path = Path();
 
   final Offset a = Offset(0, 0);
   final Offset b = Offset(0, 0);
 
-  HeaterIconic({
-    required super.offset,
-    required super.size,
-    required super.iconicStyle,
-  }) : heaterGap = size * k_heater_gap,
-       heaterTop = size * k_heater_top,
-       heaterMiddle = size * k_heater_middle,
-       heaterBottom = size * k_heater_bottom;
+  HeaterIconic({required super.offset, required super.size, required super.iconicStyle})
+    : heaterGap = size * kHeaterGap,
+      heaterLine = size * kLine,
+      lineToArc = size * kLineToArc,
+      middleLineToArc = size * kMiddleLineToArc {
+    _path
+      ..moveTo(offset.dx + heaterLine, offset.dy - heaterGap)
+      ..relativeLineTo(-lineToArc, 0)
+      ..relativeArcToPoint(
+        Offset(-size * kArc, size * kArc),
+        radius: Radius.elliptical(size * kArc, size * kArc),
+        clockwise: false,
+      )
+      ..relativeArcToPoint(
+        Offset(size * kArc, size * kArc),
+        radius: Radius.elliptical(size * kArc, size * kArc),
+        clockwise: false,
+      )
+      ..relativeLineTo(middleLineToArc, 0)
+      ..relativeArcToPoint(
+        Offset(size * kArc, size * kArc),
+        radius: Radius.elliptical(size * kArc, size * kArc),
+        clockwise: true,
+      )
+      ..relativeArcToPoint(
+        Offset(-size * kArc, size * kArc),
+        radius: Radius.elliptical(size * kArc, size * kArc),
+        clockwise: true,
+      )
+      ..relativeLineTo(-lineToArc, 0);
+  }
 
   @override
   void paint(Canvas canvas) {
-    // canvas.drawLine(
-    //   Offset(offset.dx - heaterTop, offset.dy),
-    //   Offset(offset.dx + heaterTop, offset.dy),
-    //   strokeStyle,
-    // );
-    // canvas.drawArc(Rect.fromPoints(a, b), 0, pi / 2, false, strokeStyle);
-    // canvas.drawLine(
-    //   Offset(offset.dx - heaterTop, offset.dy - heaterGap),
-    //   Offset(offset.dx + heaterTop, offset.dy - heaterGap),
-    //   strokeStyle,
-    // );
-    // canvas.drawLine(
-    //   Offset(offset.dx - heaterTop, offset.dy + heaterGap),
-    //   Offset(offset.dx + heaterTop, offset.dy + heaterGap),
-    //   strokeStyle,
-    // );
+    canvas.drawPath(_path, strokeStyle);
   }
 }
