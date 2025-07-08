ab74ce681ce8fefae3102e3e64c3a56ce6cb85fb
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-07-08 23:45:40 +0300

    make lines directions

--- lib/ui/stages/line.dart
+++ lib/ui/stages/line.dart
@@ -1,13 +1,19 @@
-import 'dart:math';
 import 'dart:ui';
 
 import 'package:studio/ui/figures/figure.dart';
 
-enum DirectionType { left, up, right, down, any }
+enum Direction { left, up, right, down, any }
 
-typedef Anchor = ({DirectionType direction, Offset offset});
+typedef Anchor = ({Direction direction, Offset offset});
 
 class Line implements Paintable {
+  final Anchor start;
+  final Anchor end;
+  final double radius;
+  final LineStyle style;
+
+  final Path _path = Path();
+
   Line({
     this.radius = 24,
     required this.start,
@@ -15,79 +21,187 @@ class Line implements Paintable {
     required this.style,
   }) {
     _path.moveTo(start.offset.dx, start.offset.dy);
-    if (end.offset.dx < start.offset.dx && end.offset.dy > start.offset.dy) {
-      _lt();
-    } else if (end.offset.dx > start.offset.dx &&
-        end.offset.dy > start.offset.dy) {
-      _tr();
-    } else if (end.offset.dx > start.offset.dx &&
-        end.offset.dy < start.offset.dy) {
-      _rb();
-    } else if (end.offset.dy < start.offset.dy &&
-        end.offset.dx < start.offset.dx) {
-      _bl();
+    switch (start.direction) {
+      case Direction.left:
+        _l();
+      case Direction.up:
+        _u();
+      case Direction.right:
+        _r();
+      case Direction.down:
+        _d();
+      case Direction.any:
+        _a();
     }
     _path.lineTo(end.offset.dx, end.offset.dy);
   }
 
-  final Path _path = Path();
+  void _l() {
+    switch (end.direction) {
+      case Direction.left:
+        _ll();
+      case Direction.up:
+        _lu();
+      case Direction.right:
+        _lr();
+      case Direction.down:
+        _ld();
+      case Direction.any:
+        _la();
+    }
+  }
 
-  final Anchor start;
-  final Anchor end;
-  final double radius;
-  final LineStyle style;
+  void _u() {
+    switch (end.direction) {
+      case Direction.left:
+        _ul();
+      case Direction.up:
+        _uu();
+      case Direction.right:
+        _ur();
+      case Direction.down:
+        _ud();
+      case Direction.any:
+        _ua();
+    }
+  }
+
+  void _r() {
+    switch (end.direction) {
+      case Direction.left:
+        _rl();
+      case Direction.up:
+        _ru();
+      case Direction.right:
+        _rr();
+      case Direction.down:
+        _rd();
+      case Direction.any:
+        _ra();
+    }
+  }
+
+  void _d() {
+    switch (end.direction) {
+      case Direction.left:
+        _dl();
+      case Direction.up:
+        _du();
+      case Direction.right:
+        _dr();
+      case Direction.down:
+        _dd();
+      case Direction.any:
+        _da();
+    }
+  }
+
+  void _a() {
+    switch (end.direction) {
+      case Direction.left:
+        _al();
+      case Direction.up:
+        _au();
+      case Direction.right:
+        _ar();
+      case Direction.down:
+        _ad();
+      case Direction.any:
+        _aa();
+    }
+  }
+
+  void _ll() {
+    _path.relativeLineTo(-radius, 0);
+    if (end.offset.dx < start.offset.dx) {
+    } else {
+      if (end.offset.dy < start.offset.dy) {
+        final dy = start.offset.dy - end.offset.dy;
+        _path.relativeArcToPoint(
+          Offset(-radius, -radius),
+          radius: Radius.circular(radius),
+        );
+        if (dy > 2 * radius) {
+          _path.relativeLineTo(0, 2 * radius - dy);
+          _path.relativeArcToPoint(
+            Offset(radius, -radius),
+            radius: Radius.circular(radius),
+          );
+        } else {}
+      } else {
+        final dy = end.offset.dy - start.offset.dy;
+        _path.relativeArcToPoint(
+          Offset(-radius, radius),
+          radius: Radius.circular(radius),
+          clockwise: false,
+        );
+        if (dy > 2 * radius) {
+          _path.relativeLineTo(0, dy - 2 * radius);
+          _path.relativeArcToPoint(
+            Offset(radius, radius),
+            radius: Radius.circular(radius),
+            clockwise: false,
+          );
+        } else {}
+      }
+    }
+  }
+
+  void _lu() {}
+
+  void _lr() {}
+
+  void _ld() {}
+
+  void _la() {}
+
+  void _ul() {}
+
+  void _uu() {}
+
+  void _ur() {}
+
+  void _ud() {}
+
+  void _ua() {}
+
+  void _rl() {}
+
+  void _ru() {}
+
+  void _rr() {}
+
+  void _rd() {}
+
+  void _ra() {}
+
+  void _dl() {}
+
+  void _du() {}
+
+  void _dr() {}
+
+  void _dd() {}
+
+  void _da() {}
+
+  void _al() {}
+
+  void _au() {}
+
+  void _ar() {}
+
+  void _ad() {}
+
+  void _aa() {}
+
+  void _lur(double dx) {}
+  void _ldr(double dx) {}
 
   @override
   void paint(Canvas canvas) {
     canvas.drawPath(_path, style.back);
     canvas.drawPath(_path, style.stroke);
-  }
-
-  void _lt() {
-    final dx = start.offset.dx - end.offset.dx;
-    final dy = end.offset.dy - start.offset.dy;
-    final rx = min(radius, dx);
-    final ry = min(radius, dy);
-    _path.relativeLineTo(rx - dx, 0);
-    _path.relativeArcToPoint(
-      Offset(-rx, ry),
-      radius: Radius.elliptical(rx, ry),
-      clockwise: false,
-    );
-  }
-
-  void _tr() {
-    final dx = end.offset.dx - start.offset.dx;
-    final dy = end.offset.dy - start.offset.dy;
-    final rx = min(radius, dx);
-    final ry = min(radius, dy);
-    _path.relativeLineTo(dx - rx, 0);
-    _path.relativeArcToPoint(Offset(rx, ry), radius: Radius.elliptical(rx, ry));
-  }
-
-  void _rb() {
-    final dx = end.offset.dx - start.offset.dx;
-    final dy = start.offset.dy - end.offset.dy;
-    final rx = min(radius, dx);
-    final ry = min(radius, dy);
-    _path.relativeLineTo(dx - rx, 0);
-    _path.relativeArcToPoint(
-      Offset(rx, -ry),
-      radius: Radius.elliptical(rx, ry),
-      clockwise: false,
-    );
-  }
-
-  void _bl() {
-    final dx = start.offset.dx - end.offset.dx;
-    final dy = start.offset.dy - end.offset.dy;
-    final rx = min(radius, dx);
-    final ry = min(radius, dy);
-    _path.relativeLineTo(rx - dx, 0);
-    _path.relativeArcToPoint(
-      Offset(-rx, -ry),
-      radius: Radius.elliptical(rx, ry),
-    );
   }
 }
 
