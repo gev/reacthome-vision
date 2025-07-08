b626939d2c7e9c2363c5cd0081e0c87a8ec4c75b
darvironia <veronika.gazdovskaya@gmail.com>
2025-07-08 17:57:21 +0300

    mixer icons

--- lib/ui/figures/iconics/mixer.dart
+++ lib/ui/figures/iconics/mixer.dart
@@ -4,23 +4,84 @@ import 'package:studio/ui/figures/iconic.dart';
 
 const kMixerGap = 0.4;
 
-class MixerIconic extends Iconic {
+abstract class MixerIconic extends Iconic {
   final double mixerGap;
   final double halfSize;
   final Path _path = Path();
 
   MixerIconic({required super.offset, required super.size, required super.iconicStyle})
     : mixerGap = size * kMixerGap,
-      halfSize = size * 0.5 {
-    _path
-      ..moveTo(offset.dx - mixerGap, offset.dy - halfSize)
-      ..relativeLineTo(mixerGap, halfSize)
-      ..relativeLineTo(0, halfSize)
-      ..moveTo(offset.dx, offset.dy)
-      ..relativeLineTo(mixerGap, -halfSize);
-  }
+      halfSize = size * 0.5;
   @override
   void paint(Canvas canvas) {
     canvas.drawPath(_path, strokeStyle);
+  }
+}
+
+class DownMixerIconic extends MixerIconic {
+  final double mixerGap;
+  final double halfSize;
+  final Path _path = Path();
+
+  DownMixerIconic({required super.offset, required super.size, required super.iconicStyle})
+    : mixerGap = size * kMixerGap,
+      halfSize = size * 0.5 {
+    _path
+      ..moveTo(offset.dx - mixerGap, offset.dy + halfSize)
+      ..relativeLineTo(mixerGap, -halfSize)
+      ..relativeLineTo(0, -halfSize)
+      ..moveTo(offset.dx, offset.dy)
+      ..relativeLineTo(mixerGap, halfSize);
+  }
+}
+
+class UpMixerIconic extends MixerIconic {
+  final double mixerGap;
+  final double halfSize;
+  final Path _path = Path();
+
+  UpMixerIconic({required super.offset, required super.size, required super.iconicStyle})
+    : mixerGap = size * kMixerGap,
+      halfSize = size * 0.5 {
+    _path
+      ..moveTo(offset.dx - mixerGap, offset.dy + halfSize)
+      ..relativeLineTo(mixerGap, -halfSize)
+      ..relativeLineTo(0, -halfSize)
+      ..moveTo(offset.dx, offset.dy)
+      ..relativeLineTo(mixerGap, halfSize);
+  }
+}
+
+class RightMixerIconic extends MixerIconic {
+  final double mixerGap;
+  final double halfSize;
+  final Path _path = Path();
+
+  RightMixerIconic({required super.offset, required super.size, required super.iconicStyle})
+    : mixerGap = size * kMixerGap,
+      halfSize = size * 0.5 {
+    _path
+      ..moveTo(offset.dx - halfSize, offset.dy + mixerGap)
+      ..relativeLineTo(halfSize, -mixerGap)
+      ..relativeLineTo(halfSize, 0)
+      ..moveTo(offset.dx, offset.dy)
+      ..relativeLineTo(-halfSize, -mixerGap);
+  }
+}
+
+class LeftMixerIconic extends MixerIconic {
+  final double mixerGap;
+  final double halfSize;
+  final Path _path = Path();
+
+  LeftMixerIconic({required super.offset, required super.size, required super.iconicStyle})
+    : mixerGap = size * kMixerGap,
+      halfSize = size * 0.5 {
+    _path
+      ..moveTo(offset.dx + halfSize, offset.dy + mixerGap)
+      ..relativeLineTo(-halfSize, -mixerGap)
+      ..relativeLineTo(-halfSize, 0)
+      ..moveTo(offset.dx, offset.dy)
+      ..relativeLineTo(halfSize, -mixerGap);
   }
 }
