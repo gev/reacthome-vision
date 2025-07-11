9c84bcffbd0778b132f15542c9c989bf278af4df
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-07-11 12:25:42 +0300

    add a link's direction

--- lib/core/link.dart
+++ lib/core/link.dart
@@ -1,8 +1,10 @@
+import 'package:studio/ui/stages/anchor_line.dart';
+
 class Connector<Id> {
-  Connector({required this.id, required this.idx});
+  Connector({required this.id, required this.direction});
 
   final Id id;
-  final int idx;
+  final Direction direction;
 }
 
 class Link<Id> {
