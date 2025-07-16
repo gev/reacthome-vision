3abc6765eee32679670295bfbba0c4b957356b32
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-07-16 19:21:31 +0300

    line from down to down

--- lib/ui/stages/lines/down_right.dart
+++ lib/ui/stages/lines/down_right.dart
@@ -7,4 +7,82 @@ class DownRight extends Line {
     required super.radius,
     required super.style,
   });
+
+  @override
+  void toRightDown(double dx, double dy) {
+    moveTo(start.dx, start.dy + radius);
+    if (dy > 4 * radius) {
+      lineToDown(dy / 2 - 2 * radius);
+      arcToDownRight();
+      lineToRight(dx);
+      arcToRightDown();
+      lineToDown(dy / 2 - 2 * radius);
+      arcToDownLeft();
+    } else {
+      lineToDown(dy);
+      arcToDownRight();
+      lineToRight(dx);
+      arcToRightUp();
+      arcToUpLeft();
+    }
+  }
+
+  @override
+  void toRightUp(double dx, double dy) {
+    moveTo(start.dx, start.dy + radius);
+    arcToDownRight();
+    lineToRight(dx);
+    arcToRightUp();
+    lineToUp(dy);
+    arcToUpLeft();
+  }
+
+  @override
+  void toLeftDown(double dx, double dy) {
+    moveTo(start.dx, start.dy + radius);
+    if (dx > 2 * radius && dy > 2 * radius) {
+      lineToDown(dy - 2 * radius);
+      arcToDownLeft();
+      lineToLeft(dx - 2 * radius);
+    } else if (dx > 4 * radius) {
+      lineToDown(dy);
+      arcToDownLeft();
+      lineToLeft(dx / 2 - 2 * radius);
+      arcToLeftUp();
+      arcToUpLeft();
+      lineToLeft(dx / 2 - 2 * radius);
+    } else if (dy > 4 * radius) {
+      lineToDown(dy / 2 - 2 * radius);
+      arcToDownRight();
+      arcToRightDown();
+      lineToDown(dy / 2 - 2 * radius);
+      arcToDownLeft();
+      lineToLeft(dx);
+    } else {
+      lineToDown(dy);
+      arcToDownRight();
+      arcToRightUp();
+      arcToUpLeft();
+      lineToLeft(dx);
+    }
+  }
+
+  @override
+  void toLeftUp(double dx, double dy) {
+    moveTo(start.dx, start.dy + radius);
+    if (dx > 4 * radius) {
+      arcToDownLeft();
+      lineToLeft(dx / 2 - 2 * radius);
+      arcToLeftUp();
+      lineToUp(dy);
+      arcToUpLeft();
+      lineToLeft(dx / 2 - 2 * radius);
+    } else {
+      arcToDownRight();
+      arcToRightUp();
+      lineToUp(dy);
+      arcToUpLeft();
+      lineToLeft(dx);
+    }
+  }
 }
