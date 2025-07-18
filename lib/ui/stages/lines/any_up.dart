1bca28aa4c5d2bd4a8e96a020cb4efbcba1e26cb
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-07-18 17:56:04 +0300

    line from any to up

--- lib/ui/stages/lines/any_up.dart
+++ lib/ui/stages/lines/any_up.dart
@@ -7,4 +7,114 @@ class AnyUp extends Line {
     required super.radius,
     required super.style,
   });
+
+  @override
+  void toRightDown(double dx, double dy) {
+    if (dx == 0 && dy > 2 * radius) {
+      lineToDown(dy - 2 * radius);
+    } else if (dx > 2 * radius && dy > 2 * radius) {
+      moveTo(start.dx + radius, start.dy);
+      lineToRight(dx - 2 * radius);
+      arcToRightDown();
+      lineToDown(dy - 2 * radius);
+    } else if (dy > 6 * radius) {
+      moveTo(start.dx, start.dy + radius);
+      lineToDown(dy / 3 - 2 * radius);
+      arcToDownLeft();
+      arcToLeftDown();
+      lineToDown(dy / 3 - 2 * radius);
+      arcToDownRight();
+      lineToRight(dx);
+      arcToRightDown();
+      lineToDown(dy / 3 - 2 * radius);
+    } else if (dy > 4 * radius) {
+      moveTo(start.dx - radius, start.dy);
+      arcToLeftDown();
+      lineToDown(dy / 2 - 2 * radius);
+      arcToDownRight();
+      lineToRight(dx);
+      arcToRightDown();
+      lineToDown(dy / 2 - 2 * radius);
+    } else {
+      moveTo(start.dx - radius, start.dy);
+      arcToLeftUp();
+      arcToUpRight();
+      lineToRight(dx);
+      arcToRightDown();
+      lineToDown(dy);
+    }
+  }
+
+  @override
+  void toRightUp(double dx, double dy) {
+    if (dx > 2 * radius) {
+      moveTo(start.dx, start.dy - radius);
+      lineToUp(dy);
+      arcToUpRight();
+      lineToRight(dx - 2 * radius);
+    } else {
+      moveTo(start.dx, start.dy + radius);
+      arcToDownLeft();
+      arcToLeftUp();
+      lineToUp(dy);
+      arcToUpRight();
+      lineToRight(dx);
+      arcToRightDown();
+    }
+  }
+
+  @override
+  void toLeftDown(double dx, double dy) {
+    if (dx == 0 && dy > 2 * radius) {
+      lineToDown(dy - 2 * radius);
+    } else if (dx > 2 * radius && dy > 2 * radius) {
+      moveTo(start.dx + radius, start.dy);
+      lineToLeft(dx - 2 * radius);
+      arcToLeftDown();
+      lineToDown(dy - 2 * radius);
+    } else if (dy > 6 * radius) {
+      moveTo(start.dx, start.dy + radius);
+      lineToDown(dy / 3 - 2 * radius);
+      arcToDownRight();
+      arcToRightDown();
+      lineToDown(dy / 3 - 2 * radius);
+      arcToDownLeft();
+      lineToLeft(dx);
+      arcToLeftDown();
+      lineToDown(dy / 3 - 2 * radius);
+    } else if (dy > 4 * radius) {
+      moveTo(start.dx - radius, start.dy);
+      arcToRightDown();
+      lineToDown(dy / 2 - 2 * radius);
+      arcToDownLeft();
+      lineToLeft(dx);
+      arcToLeftDown();
+      lineToDown(dy / 2 - 2 * radius);
+    } else {
+      moveTo(start.dx - radius, start.dy);
+      arcToRightUp();
+      arcToUpLeft();
+      lineToLeft(dx);
+      arcToLeftDown();
+      lineToDown(dy);
+    }
+  }
+
+  @override
+  void toLeftUp(double dx, double dy) {
+    if (dx > 2 * radius) {
+      moveTo(start.dx, start.dy - radius);
+      lineToUp(dy);
+      arcToUpLeft();
+      lineToLeft(dx - 2 * radius);
+    } else {
+      moveTo(start.dx, start.dy + radius);
+      arcToDownRight();
+      arcToRightUp();
+      lineToUp(dy);
+      arcToUpLeft();
+      lineToLeft(dx);
+      arcToLeftDown();
+    }
+  }
 }
