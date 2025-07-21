656840541f0394baea0dd64bb0465e23250b4902
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-07-21 14:11:20 +0300

    separate studio application into  package

--- apps/studio/lib/main.dart
+++ apps/studio/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/widgets.dart';
-import 'package:macos_ui/macos_ui.dart';
+// import 'package:macos_ui/macos_ui.dart';
 import 'package:studio/make/make.dart';
 
-Future<void> configureMacosWindowUtils() async {
-  const config = MacosWindowUtilsConfig();
-  await config.apply();
-}
+// Future<void> configureMacosWindowUtils() async {
+//   const config = MacosWindowUtilsConfig();
+//   await config.apply();
+// }
 
 void main() async {
   // if (Platform.isMacOS) {
