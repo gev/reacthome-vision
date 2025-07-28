1982a8f0fe63b9e607504fb0dc1fb7b0a871cc47
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-07-28 16:51:21 +0300

    stylish

--- packages/studio/lib/make/make.dart
+++ packages/studio/lib/make/make.dart
@@ -2,8 +2,8 @@ import 'package:flutter/widgets.dart';
 import 'package:studio/make/config.dart';
 import 'package:studio/make/screens/splash_screen_factory.dart';
 import 'package:studio/make/screens/welcome_screen_factory.dart';
-import 'package:ui_kit/navigation.dart';
 import 'package:studio/ui/views/app.dart';
+import 'package:ui_kit/navigation.dart';
 
 Future<Widget> make() async {
   return App(
