97030d1ec597f5177278237647ecc6ca10dea7e5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-01 22:59:05 +0300

    remove material dependency

--- packages/ui_kit_cupertino/lib/theme_cupertino.dart
+++ packages/ui_kit_cupertino/lib/theme_cupertino.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:ui_kit/theme.dart' as ui_kit;
-import 'package:ui_kit_material/theme_material.dart';
 
 class ThemeCupertino implements ui_kit.Theme {
   late final CupertinoThemeData _themeData;
@@ -30,16 +28,11 @@ class ThemeCupertino implements ui_kit.Theme {
 }
 
 CupertinoThemeData makeCupertinoTheme(Color seedColor, Brightness brightness) =>
-    MaterialBasedCupertinoThemeData(
-      materialTheme: makeMaterialTheme(
-        seedColor,
-        brightness,
-        CupertinoThemeData(
-          brightness: brightness,
-          textTheme: const CupertinoTextThemeData(),
-          barBackgroundColor: CupertinoColors.systemBackground,
-          scaffoldBackgroundColor: CupertinoColors.systemGroupedBackground,
-          applyThemeToAll: true,
-        ),
-      ),
+    CupertinoThemeData(
+      primaryColor: seedColor,
+      brightness: brightness,
+      textTheme: const CupertinoTextThemeData(),
+      barBackgroundColor: CupertinoColors.systemBackground,
+      scaffoldBackgroundColor: CupertinoColors.systemGroupedBackground,
+      applyThemeToAll: true,
     );
