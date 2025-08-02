d57fc085391d8826de2de1d5581d718d474fa05a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-02 19:35:28 +0300

    add fluent ui kit

--- apps/flow/windows/lib/main.dart
+++ apps/flow/windows/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flow/make/make.dart';
 import 'package:flutter/widgets.dart';
-import 'package:ui_kit_material/init.dart';
+import 'package:ui_kit_fluent/init.dart';
 
 void main() async {
-  initMaterialKit();
+  initFluentKit();
   runApp(await make());
 }
