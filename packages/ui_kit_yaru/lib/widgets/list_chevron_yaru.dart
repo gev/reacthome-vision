d8b32a4dac8c1bfecdd6f15d46a03d6f14c78be6
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-04 21:10:53 +0300

    yaru list chevron

---
+++ packages/ui_kit_yaru/lib/widgets/list_chevron_yaru.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:yaru/icons.dart';
+
+class ListChevronYaru extends StatelessWidget {
+  const ListChevronYaru({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(YaruIcons.go_next);
+  }
+}
