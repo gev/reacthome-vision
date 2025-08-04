d5b095163270b5e16e24dc21fd06858056ed545b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-04 18:44:11 +0300

    up

--- apps/flow/macos/lib/main.dart
+++ apps/flow/macos/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flow/make/make.dart';
 import 'package:flutter/widgets.dart';
-import 'package:ui_kit_yaru/init.dart';
 // import 'package:ui_kit_macos/init.dart';
+import 'package:ui_kit_yaru/init.dart';
 
 void main() async {
   // await configureMacosWindowUtils();
