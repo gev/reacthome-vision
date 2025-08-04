d5a02ed8a6934a49d99a9f33449a2e6dba203fd7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-04 04:05:13 +0300

    clean

--- apps/flow/macos/lib/main.dart
+++ apps/flow/macos/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flow/make/make.dart';
 import 'package:flutter/widgets.dart';
-// import 'package:ui_kit_macos/init.dart';
-import 'package:ui_kit_fluent/init.dart';
+import 'package:ui_kit_macos/init.dart';
 
 void main() async {
-  // await configureMacosWindowUtils();
-  // initMacOSKit();
-  initFluentKit();
+  await configureMacosWindowUtils();
+  initMacOSKit();
   runApp(await make());
 }
