534d8ef2469785e8cfcd2647ebf7639d8ee8a37c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-05 00:50:53 +0300

    cleanup

--- apps/flow/macos/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/flow/macos/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,6 @@ import connectivity_plus
 import macos_ui
 import macos_window_utils
 import path_provider_foundation
-import screen_retriever_macos
-import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppkitUiElementColorsPlugin.register(with: registry.registrar(forPlugin: "AppkitUiElementColorsPlugin"))
@@ -19,6 +17,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
-  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
