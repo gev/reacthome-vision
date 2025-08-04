6af4d72a04e7595c0aa1bb9e0a9fba5e4a56d852
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-04 03:50:00 +0300

    fluent icons

--- packages/ui_kit_fluent/lib/init.dart
+++ packages/ui_kit_fluent/lib/init.dart
@@ -1,8 +1,8 @@
 import 'package:ui_kit/widgets.dart';
+import 'package:ui_kit_fluent/icons_fluent.dart';
 import 'package:ui_kit_fluent/widgets_fluent.dart';
-import 'package:ui_kit_material/icons_material.dart';
 
 void initFluentKit() {
   UI = const WidgetsFluent();
-  Icons = const IconsMaterial();
+  Icons = const IconsFluent();
 }
