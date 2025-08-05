fb45ec9f8564ddea293c37c7888e7d3787fa1fa5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-05 23:47:43 +0300

    typo

--- apps/flow/macos/lib/main.dart
+++ apps/flow/macos/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:ui_kit_macos/init.dart';
 
 void main() async {
-  await configureMacosWindowUtils();
+  await configureMacosWindow();
   initMacOSKit();
   runApp(await make());
 }
