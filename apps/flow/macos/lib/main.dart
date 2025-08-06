9f000056f8649996e523969387b0a339699c8de5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-06 21:07:36 +0300

    init macos window

--- apps/flow/macos/lib/main.dart
+++ apps/flow/macos/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:ui_kit_macos/init.dart';
 
 void main() async {
+  await configureMacosWindow();
   initMacOSKit();
   runApp(await make());
 }
