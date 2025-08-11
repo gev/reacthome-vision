875fd9d6a3a9e38c7a9c6d16875511f49bb9c0d5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-11 22:15:57 +0300

    yaru init

--- packages/ui_kit_yaru/lib/init.dart
+++ packages/ui_kit_yaru/lib/init.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:ui_kit/widgets.dart';
 import 'package:ui_kit_yaru/icons_yaru.dart';
 import 'package:ui_kit_yaru/widgets_yaru.dart';
@@ -5,6 +7,8 @@ import 'package:yaru/yaru.dart';
 
 Future<void> configureYaruWindow() async {
   await YaruWindowTitleBar.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  SemanticsBinding.instance.ensureSemantics();
 }
 
 void initYaruKit() {
