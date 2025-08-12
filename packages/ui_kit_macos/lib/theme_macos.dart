253c5b10443346e693b90ee57ca7ea09fdef6591
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-12 23:21:50 +0300

    macos theme background

--- packages/ui_kit_macos/lib/theme_macos.dart
+++ packages/ui_kit_macos/lib/theme_macos.dart
@@ -3,29 +3,31 @@ import 'package:macos_ui/macos_ui.dart';
 import 'package:ui_kit/theme.dart' as ui_kit;
 
 class ThemeMacos implements ui_kit.Theme {
-  late final MacosThemeData _themeData;
+  late final MacosThemeData _themeMacos;
+  late final CupertinoThemeData _themeCupertino;
 
   ThemeMacos(BuildContext context) {
-    _themeData = MacosTheme.of(context);
+    _themeMacos = MacosTheme.of(context);
+    _themeCupertino = CupertinoTheme.of(context);
   }
 
   @override
-  TextStyle get titleStyle => _themeData.typography.largeTitle;
+  TextStyle get titleStyle => _themeMacos.typography.largeTitle;
 
   @override
-  Color get color => _themeData.typography.body.color!;
+  Color get color => _themeMacos.typography.body.color!;
 
   @override
-  TextStyle get bodyStyle => _themeData.typography.body;
+  TextStyle get bodyStyle => _themeMacos.typography.body;
 
   @override
-  Color get backgroundColor => _themeData.canvasColor;
+  Color get backgroundColor => _themeCupertino.barBackgroundColor;
 
   @override
-  Color get primaryColor => _themeData.primaryColor;
+  Color get primaryColor => _themeMacos.primaryColor;
 
   @override
-  Color get secondaryColor => _themeData.dividerColor;
+  Color get secondaryColor => _themeMacos.dividerColor;
 }
 
 MacosThemeData makeMacOSTheme(
@@ -36,4 +38,5 @@ MacosThemeData makeMacOSTheme(
   // primaryColor: seedColor,
   accentColor: accentColor,
   brightness: brightness,
+  canvasColor: MacosColors.transparent,
 );
