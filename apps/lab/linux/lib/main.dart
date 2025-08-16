8f4b1cf9c82796bbdc3c8f08a04ce2ceb4fddb71
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-16 17:33:30 +0300

    Linux`Lab`

---
+++ apps/lab/linux/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:lab/make/make.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ui_kit_yaru/init.dart';
+
+void main() async {
+  initYaruKit();
+  runApp(await make());
+}
