a5dbdd936da5662a33fdc2d0243fbd7081a6010a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-16 17:33:04 +0300

    Android `Lab`

---
+++ apps/lab/android/android/app/src/main/kotlin/net/reacthome/flow/MainActivity.kt
@@ -0,0 +1,5 @@
+package net.reacthome.lab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/lab/android/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:lab/make/make.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ui_kit_material/init.dart';
+
+void main() async {
+  initMaterialKit();
+  runApp(await make());
+}
