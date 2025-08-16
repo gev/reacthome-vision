0778f853939fc39c2b2a094553779de151ef4ce1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-17 02:09:04 +0300

    typo

--- packages/ui_kit_material/lib/widgets/app_bar_material.dart
+++ packages/ui_kit_material/lib/widgets/app_bar_material.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppBarMaterial extends AppBar {
   AppBarMaterial({required String title, Widget? trailing, super.key})
-    : super(title: Text(title), actions: trailing != null ? [trailing] : []);
+    : super(title: Text(title), actions: [?trailing]);
 }
--- packages/ui_kit_yaru/lib/widgets/app_bar_yaru.dart
+++ packages/ui_kit_yaru/lib/widgets/app_bar_yaru.dart
@@ -3,5 +3,5 @@ import 'package:yaru/yaru.dart';
 
 class AppBarYaru extends YaruWindowTitleBar {
   AppBarYaru({required String title, Widget? trailing, super.key})
-    : super(title: Text(title), actions: trailing != null ? [trailing] : []);
+    : super(title: Text(title), actions: [?trailing]);
 }
