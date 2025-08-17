bcb54994de098844ec5302c454ab94d4fb29c896
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-17 23:05:21 +0300

    init yaru window

--- apps/flow/linux/lib/main.dart
+++ apps/flow/linux/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flow/make/make.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ui_kit/layouts/desktop/window_size.dart';
 import 'package:ui_kit_yaru/init.dart';
 
 void main() async {
+  await configureYaruWindow(windowSize);
   initYaruKit();
   runApp(await make());
 }
--- apps/lab/linux/lib/main.dart
+++ apps/lab/linux/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:lab/make/make.dart';
+import 'package:ui_kit/layouts/desktop/window_size.dart';
 import 'package:ui_kit_yaru/init.dart';
 
 void main() async {
+  await configureYaruWindow(windowSize);
   initYaruKit();
   runApp(await make());
 }
