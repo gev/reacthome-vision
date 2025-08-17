d6516d42cbd6005fd9c088923c15711432b51e58
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-08-17 15:48:10 +0300

    cupertino divider

--- packages/ui_kit_cupertino/lib/widgets/divider_cupertino.dart
+++ packages/ui_kit_cupertino/lib/widgets/divider_cupertino.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
+import 'package:ui_kit/widgets.dart';
 
 class DividerCupertino extends StatelessWidget {
   const DividerCupertino({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    final theme = UI.themeOf(context);
+    return Container(height: 1, color: theme.color.withAlpha(32));
   }
 }
