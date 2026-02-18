e73f6b1056676a50752bebfaefbc25a1e3836f2f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-02-18 22:27:16 +0300

    Add `vision package`
    Vision is a template of an application

---
+++ packages/vision/lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  final String title;
+  const App({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(body: Center(child: Text('Vision'))),
+    );
+  }
+}
