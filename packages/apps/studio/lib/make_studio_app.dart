2b7d1b93af9017e5c45a3fe07520bdf3f52ce903
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-02-20 20:49:52 +0300

    Adopt apps to the new `vision` version

--- packages/apps/studio/lib/make_studio_app.dart
+++ packages/apps/studio/lib/make_studio_app.dart
@@ -1,6 +1,23 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:vision/controllers/glue_inbound_controller.dart';
+import 'package:vision/controllers/glue_outbound_controller.dart';
+import 'package:vision/uuid.dart';
 import 'package:vision/vision_app.dart';
+import 'package:vision/websocket/websocket_client.dart';
 
 Widget makeStudioApp() {
-  return VisionApp(title: 'Studio');
+  final source = StreamController<Uint8List>();
+  final sink = StreamController<String>();
+  GlueInboundController(source.stream.map(utf8.decode));
+  GlueOutboundController(sink);
+  final client = WebSocketClient(
+    url: 'ws://127.0.0.1:3005/${uuid()}',
+    sink: source,
+    source: sink.stream,
+  );
+  return VisionApp(title: 'Lab', client: client);
 }
