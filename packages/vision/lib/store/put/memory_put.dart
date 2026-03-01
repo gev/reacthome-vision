daa3b2ca7c03bf30b34f24a8f27ea2c1dbbf9706
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-03-01 16:45:44 +0300

    Rename InMemoryPut to the MemoryPut

--- packages/vision/lib/store/put/memory_put.dart
+++ packages/vision/lib/store/put/memory_put.dart
@@ -1,10 +1,10 @@
 import 'package:vision/store/internal_store.dart';
 import 'package:vision/store/put.dart';
 
-class InMemoryPut<K, V> implements Put<K, V> {
+class MemoryPut<K, V> implements Put<K, V> {
   final InternalStore<K, V> _store;
 
-  InMemoryPut(this._store);
+  MemoryPut(this._store);
 
   @override
   void put(K key, V value) {
