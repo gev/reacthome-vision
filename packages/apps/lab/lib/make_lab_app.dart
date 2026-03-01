7d781b8818ea329fa2a6383d34f07c707000bfb1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-03-01 22:11:47 +0300

    Refactor applications

--- packages/apps/lab/lib/make_lab_app.dart
+++ packages/apps/lab/lib/make_lab_app.dart
@@ -1,23 +1,6 @@
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:vision/controllers/glue_inbound_controller.dart';
-import 'package:vision/controllers/glue_outbound_controller.dart';
+import 'package:vision/app.dart';
 import 'package:vision/uuid.dart';
-import 'package:vision/vision_app.dart';
-import 'package:vision/websocket/websocket_client.dart';
 
-Widget makeLabApp() {
-  final source = StreamController<Uint8List>();
-  final sink = StreamController<String>();
-  GlueInboundController(source.stream.map(utf8.decode));
-  GlueOutboundController(sink);
-  final client = WebSocketClient(
-    url: 'ws://127.0.0.1:3005/${uuid()}',
-    sink: source,
-    source: sink.stream,
-  );
-  return VisionApp(title: 'Lab', client: client);
-}
+Widget makeLabApp() =>
+    makeApp(id: uuid(), title: 'Lab', host: '127.0.0.1', port: 3005);
