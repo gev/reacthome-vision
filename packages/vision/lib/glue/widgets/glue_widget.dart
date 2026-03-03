9970746ac17a32df8d93f856d0f765a370ad1ea2
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-03-04 01:40:07 +0300

    Get `Evaluator` from the context

--- packages/vision/lib/glue/widgets/glue_widget.dart
+++ packages/vision/lib/glue/widgets/glue_widget.dart
@@ -3,17 +3,17 @@ import 'package:glue/either.dart';
 import 'package:glue/error.dart';
 import 'package:glue/ir.dart';
 import 'package:vision/glue/extract.dart';
-import 'package:vision/glue/glue_evaluator.dart';
+import 'package:vision/vision_scope.dart';
 import 'package:vision/widgets/empty_widget.dart';
 
 class GlueWidget extends StatelessWidget {
   final String glue;
-  final GlueEvaluator evaluator;
 
-  const GlueWidget({required this.glue, required this.evaluator, super.key});
+  const GlueWidget({required this.glue, super.key});
 
   @override
   Widget build(BuildContext context) {
+    final evaluator = VisionScope.of(context).evaluator;
     return FutureBuilder<Either<GlueError, Ir>>(
       initialData: Right(IrNativeValue(Value(EmptyWidget()))),
       future: evaluator.evaluate(glue),
