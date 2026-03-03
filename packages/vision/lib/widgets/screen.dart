9f7dc24dac15a4900a376e532f7c7a1cdad13e5c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-03-04 01:41:20 +0300

    Clean up the `Screen`

--- packages/vision/lib/widgets/screen.dart
+++ packages/vision/lib/widgets/screen.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:vision/websocket/websocket_client.dart';
 import 'package:vision/widgets/statusbar.dart';
 
 class Screen extends StatelessWidget {
-  final WebSocketClient client;
-  final Widget child;
-  const Screen({required this.client, required this.child, super.key});
+  final Widget body;
+  const Screen({required this.body, super.key});
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
       body: Column(
         children: [
-          Expanded(child: child),
-          Statusbar(
-            client: client,
-            defaultColor: colorScheme.secondaryContainer,
-            accentColor: colorScheme.errorContainer,
-          ),
+          Expanded(child: body),
+          const Statusbar(),
         ],
       ),
     );
