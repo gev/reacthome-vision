d3ba38fb47a705ea6463e470cebbe83b3b19118d
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-03-09 11:55:12 +0300

    Return Screen widget (awhile)

--- packages/vision/lib/navigation/app_route.dart
+++ packages/vision/lib/navigation/app_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vision/widgets/screen.dart';
 
 Route<T> makeRoute<T>(Widget widget) {
-  return MaterialPageRoute(builder: (_) => widget);
+  return MaterialPageRoute(builder: (_) => Screen(body: widget));
 }
