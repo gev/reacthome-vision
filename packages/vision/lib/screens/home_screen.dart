47defdd6234153e4b0ea7f88e91544018f07fc64
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-03-23 15:30:42 +0300

    Wrap `HomeScreen` into `Screen`

--- packages/vision/lib/screens/home_screen.dart
+++ packages/vision/lib/screens/home_screen.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:vision/glue/widgets/glue_widget.dart';
+import 'package:vision/widgets/screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   final _main = '''
-    (listen 
+    (listen
       (lookup store.cache "main" placeholder)
       (lambda (main) main))
   ''';
 
   @override
   Widget build(BuildContext context) {
-    return GlueWidget(glue: _main);
+    return Screen(body: GlueWidget(glue: _main));
   }
 }
