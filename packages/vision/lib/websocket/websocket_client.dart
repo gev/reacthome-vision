8d39432259a0e9d24cb205537dc65c98b11ebd77
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-15 01:51:29 +0300

    Simplify WebsocketClient

--- packages/vision/lib/websocket/websocket_client.dart
+++ packages/vision/lib/websocket/websocket_client.dart
@@ -2,22 +2,18 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:vision/websocket/websocket_reconnect_policy.dart';
 import 'package:vision/websocket/websocket_state.dart';
 
 class WebSocketClient extends ChangeNotifier {
   final String _url;
   final Sink<Uint8List> _sink;
   final Stream<Uint8List> _source;
-  final WebSocketReconnectPolicy _reconnectPolicy;
 
   late final StreamSubscription<Uint8List> _subscription;
 
   WebSocket? _socket;
 
   WebSocketConnectionState _state = WebSocketConnectionState.disconnected;
-  bool _isManuallyDisconnected = false;
-  Timer? _reconnectTimer;
 
   WebSocketConnectionState get state => _state;
 
@@ -25,18 +21,14 @@ class WebSocketClient extends ChangeNotifier {
     required String url,
     required Sink<Uint8List> sink,
     required Stream<Uint8List> source,
-    WebSocketReconnectPolicy? reconnectPolicy,
   }) : _url = url,
        _sink = sink,
-       _source = source,
-       _reconnectPolicy = reconnectPolicy ?? WebSocketReconnectPolicy() {
+       _source = source {
     _subscription = _source.listen(_sendMessage);
     _establishConnection();
   }
 
   Future<void> disconnect() async {
-    _reconnectTimer?.cancel();
-    _isManuallyDisconnected = true;
     _updateState(WebSocketConnectionState.disconnected);
     _closeSocket();
   }
@@ -45,7 +37,6 @@ class WebSocketClient extends ChangeNotifier {
     _updateState(WebSocketConnectionState.connecting);
     try {
       _socket = await WebSocket.connect(_url);
-      _reconnectPolicy.recordSuccess();
       _updateState(WebSocketConnectionState.connected);
       _socket!.listen(
         _receiveMessage,
@@ -67,19 +58,7 @@ class WebSocketClient extends ChangeNotifier {
 
   void _onConnectionLost() {
     _updateState(WebSocketConnectionState.disconnected);
-    _scheduleReconnect();
     _socket = null;
-  }
-
-  void _scheduleReconnect() {
-    if (!_isManuallyDisconnected && _reconnectPolicy.canRetry) {
-      final delay = _reconnectPolicy.nextDelayMs;
-      _reconnectPolicy.recordFailedAttempt();
-      _reconnectTimer = Timer(
-        Duration(milliseconds: delay),
-        _establishConnection,
-      );
-    }
   }
 
   void _updateState(WebSocketConnectionState newState) {
@@ -91,7 +70,6 @@ class WebSocketClient extends ChangeNotifier {
 
   @override
   void dispose() {
-    _reconnectTimer?.cancel();
     _subscription.cancel();
     _closeSocket();
     super.dispose();
