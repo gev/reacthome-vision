806bbe9e1f27e07b4d1f9ad1cd36c476b58243c5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-18 01:26:37 +0300

    Add `ResilientWebSocket`

---
+++ packages/vision/lib/websocket/resilient_websocket.dart
@@ -0,0 +1,30 @@
+import 'dart:typed_data';
+
+import 'package:vision/retry/retry.dart';
+import 'package:vision/websocket/retryable_websocket.dart';
+
+class ResilientWebSocket {
+  late final Retry _retry;
+
+  ResilientWebSocket({
+    required String url,
+    required Sink<Uint8List> sink,
+    required Stream<Uint8List> source,
+    required OnWebSocketChangeState onStateChange,
+    required RetryPolicy policy,
+  }) {
+    _retry = Retry(
+      process: RetryableWebSocket(
+        url: url,
+        sink: sink,
+        source: source,
+        onStateChange: onStateChange,
+      ),
+      policy: policy,
+    );
+  }
+
+  Future<void> start() async {
+    _retry.start();
+  }
+}
