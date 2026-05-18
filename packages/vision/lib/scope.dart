0ecbba61bb897efae924e3167302f5f5b7f53c0c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-19 00:01:16 +0300

    Use resilient WebSocket

--- packages/vision/lib/scope.dart
+++ packages/vision/lib/scope.dart
@@ -5,10 +5,11 @@ import 'dart:typed_data';
 import 'package:vision/controllers/controller.dart';
 import 'package:vision/glue/env.dart';
 import 'package:vision/glue/glue_evaluator.dart';
-import 'package:vision/websocket/websocket_client.dart';
+import 'package:vision/retry/exponentional_backoff_policy.dart';
+import 'package:vision/websocket/resilient_websocket.dart';
 
 class Scope {
-  late final WebSocketClient _client;
+  late final ResilientWebSocket _client;
   late final GlueEvaluator _evaluator;
   late final Controller _controller;
 
@@ -19,9 +20,10 @@ class Scope {
     final env = makeEnv(_outbound);
     _evaluator = GlueEvaluator(env);
     _controller = Controller(evaluator: _evaluator, source: _inbound.stream);
-    _client = WebSocketClient(
+    _client = ResilientWebSocket(
       url: 'ws://$host:$port',
       sink: _inbound,
+      policy: ExponentialBackoffPolicy(),
       source: _outbound.stream.map(
         (message) =>
             (BytesBuilder(copy: false)
@@ -30,9 +32,10 @@ class Scope {
                 .takeBytes(),
       ),
     );
+    _client.start();
   }
 
-  WebSocketClient get client => _client;
+  ResilientWebSocket get client => _client;
   GlueEvaluator get evaluator => _evaluator;
 
   void dispose() {
