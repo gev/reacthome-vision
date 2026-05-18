e1dc08b23ce7e67997e121564e93a1506667df4f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-18 23:59:40 +0300

    Add `Screen` `AppBar`

--- packages/vision/lib/glue/lib/widgets/screen.dart
+++ packages/vision/lib/glue/lib/widgets/screen.dart
@@ -1,10 +1,22 @@
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
-import 'package:vision/glue/extract.dart';
+import 'package:glue_flutter/glue_flutter.dart';
 import 'package:vision/widgets/screen.dart';
 
 final screen = IrNativeFunc(screenImpl);
 
-Eval<Ir> screenImpl(Ir ir) {
-  return Eval.pure(IrNativeValue(Value(Screen(body: extractWidget(ir)))));
+Eval<Ir> screenImpl(Ir props) => switch (props) {
+  IrObject(:final properties) => _createScreen(
+    WidgetProperties(properties.unlock),
+  ),
+  _ => _createScreen(WidgetProperties.empty()),
+};
+
+Eval<Ir> _createScreen(WidgetProperties properties) {
+  final widget = Screen(
+    key: properties.key,
+    appBar: properties.getValue('app-bar'),
+    body: properties.getWidget('body'),
+  );
+  return Eval.pure(IrNativeValue(Value(widget)));
 }
