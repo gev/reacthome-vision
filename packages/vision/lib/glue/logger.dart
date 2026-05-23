46083ab32b4ed454ace3dff221c2c3651929fe22
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-23 22:17:51 +0300

    Logger send messages to a server

---
+++ packages/vision/lib/glue/logger.dart
@@ -0,0 +1,42 @@
+import 'package:glue/ast.dart';
+import 'package:glue/serialize.dart';
+
+class LogTag {
+  static const info = "info";
+  static const debug = "debug";
+  static const error = "error";
+}
+
+class Logger {
+  final Sink<String> _sink;
+
+  Logger({required this._sink});
+
+  void info<M>(M message) {
+    log(message, tag: LogTag.info);
+  }
+
+  void debug<M>(M message) {
+    log(message, tag: LogTag.debug);
+  }
+
+  void error<M>(M message) {
+    log(message, tag: LogTag.error);
+  }
+
+  void log<M>(M message, {required String tag}) {
+    _sink.add(
+      serializeAst(
+        ListAst([
+          _log,
+          ObjectAst({
+            "tag": StringAst(tag),
+            "message": StringAst(message.toString()),
+          }),
+        ]),
+      ),
+    );
+  }
+
+  static const _log = SymbolAst('log');
+}
