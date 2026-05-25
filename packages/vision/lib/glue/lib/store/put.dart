eb83ede8cf60858acc37d380f851bc74ba4063d1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-05-26 01:56:04 +0300

    stylish

--- packages/vision/lib/glue/lib/store/put.dart
+++ packages/vision/lib/glue/lib/store/put.dart
@@ -6,12 +6,18 @@ import 'package:vision/stores/store.dart';
 
 final Ir put = IrSpecial(putImpl);
 
-Eval<Ir> putImpl(List<Ir> args) => switch (args) {
-  [final store, final key, final value] => eval(
-    store,
-  ).flatMap((s) => eval(key).flatMap((k) => putValue(s, k, value))),
-  _ => throwError(wrongArgumentType(['store', 'key', 'value'])),
-};
+Eval<Ir> putImpl(List<Ir> args) {
+  switch (args) {
+    case [final store, final key, final value]:
+      return eval(store).flatMap((s) {
+        return eval(key).flatMap((k) {
+          return putValue(s, k, value);
+        });
+      });
+    case _:
+      return throwError(wrongArgumentType(['store', 'key', 'value']));
+  }
+}
 
 Eval<Ir> putValue(Ir store, Ir key, Ir value) {
   switch ((store, key)) {
