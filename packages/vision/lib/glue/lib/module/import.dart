2c2f864bc926e333b6b61c690559fd95ca28035f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-07 16:31:48 +0300

    Use importModule for module name resolution

--- packages/vision/lib/glue/lib/module/import.dart
+++ packages/vision/lib/glue/lib/module/import.dart
@@ -1,6 +1,7 @@
 import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:glue/module/import.dart';
 import 'package:vision/glue/pub_sub/glue_subscriber.dart';
 import 'package:vision/glue/stores/reactive_runtime.dart';
 
@@ -10,7 +11,7 @@ Ir import(GlueSubscriber subscriber, ReactiveRuntime runtime) {
       case [IrSymbol(value: final name)]:
       case [IrDottedSymbol(value: final name)]:
         subscriber.subscribe(name, runtime);
-        return Eval.pure(IrVoid());
+        return importModule(name);
       default:
         return throwError(wrongArgumentType(['module name required']));
     }
