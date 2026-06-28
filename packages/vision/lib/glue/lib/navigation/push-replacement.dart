30035d5305d69f58c407e91df7f000e767cd0246
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-28 23:16:41 +0300

    Support route objects with arguments in navigation
    Update push and pushReplacement to accept route objects with 'route'
    property and other named arguments.

--- packages/vision/lib/glue/lib/navigation/push-replacement.dart
+++ packages/vision/lib/glue/lib/navigation/push-replacement.dart
@@ -5,18 +5,41 @@ import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 
 /// Replaces the current route with a new Route
-final Ir pushReplacement = IrNativeFunc((Ir routeIr) {
-  return switch (routeIr) {
-    IrSymbol(value: final route) => getRuntime().bind((runtime) {
-      final context = getFromContext<BuildContext>(runtime.context);
-      return Eval.pure(
-        context != null
-            ? IrNativeValue(
-                Value(Navigator.of(context).pushReplacementNamed(route)),
-              )
-            : IrVoid(),
-      );
-    }),
-    _ => throwError(wrongArgumentType(['route'])),
-  };
+final Ir pushReplacement = IrNativeFunc((Ir ir) {
+  return getRuntime().bind((runtime) {
+    final context = getFromContext<BuildContext>(runtime.context);
+    if (context != null) {
+      switch (ir) {
+        case IrObject(:final properties):
+          Ir? route;
+          final args = <String, Ir>{};
+          for (final entry in properties.entries) {
+            if (entry.key == 'route') {
+              route = entry.value;
+            } else {
+              args[entry.key] = entry.value;
+            }
+          }
+          switch (route) {
+            case (IrSymbol(:final value)):
+              return Eval.pure(
+                IrNativeValue(
+                  Value(
+                    Navigator.of(
+                      context,
+                    ).pushReplacementNamed(value, arguments: IrObject(args)),
+                  ),
+                ),
+              );
+            default:
+              break;
+          }
+        default:
+          return throwError(
+            wrongArgumentType(['Object with `route` required ']),
+          );
+      }
+    }
+    return Eval.pure(IrVoid());
+  });
 });
--- packages/vision/lib/glue/lib/navigation/push.dart
+++ packages/vision/lib/glue/lib/navigation/push.dart
@@ -5,16 +5,43 @@ import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 
 /// Pushes a Route onto the navigation stack
-final Ir push = IrNativeFunc((Ir routeIr) {
-  return switch (routeIr) {
-    IrSymbol(value: final route) => getRuntime().bind((runtime) {
-      final context = getFromContext<BuildContext>(runtime.context);
-      return Eval.pure(
-        context != null
-            ? IrNativeValue(Value(Navigator.of(context).pushNamed(route)))
-            : IrVoid(),
-      );
-    }),
-    _ => throwError(wrongArgumentType(['route'])),
-  };
+final Ir push = IrNativeFunc((Ir ir) {
+  return getRuntime().bind((runtime) {
+    final context = getFromContext<BuildContext>(runtime.context);
+    if (context != null) {
+      switch (ir) {
+        case IrObject(:final properties):
+          Ir? route;
+          final args = <String, Ir>{};
+          for (final entry in properties.entries) {
+            print(entry);
+            if (entry.key == 'route') {
+              route = entry.value;
+            } else {
+              args[entry.key] = entry.value;
+            }
+          }
+          switch (route) {
+            case (IrSymbol(:final value)):
+              return Eval.pure(
+                IrNativeValue(
+                  Value(
+                    Navigator.of(
+                      context,
+                    ).pushNamed(value, arguments: IrObject(args)),
+                  ),
+                ),
+              );
+            default:
+              break;
+          }
+
+        default:
+          return throwError(
+            wrongArgumentType(['Object with `route` required ']),
+          );
+      }
+    }
+    return Eval.pure(IrVoid());
+  });
 });
