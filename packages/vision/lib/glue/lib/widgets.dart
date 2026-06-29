190ab5a0c902ac0f3e5c36ae1169783b1a0c5315
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-29 22:44:00 +0300

    Add canvas widget to widgets module

--- packages/vision/lib/glue/lib/widgets.dart
+++ packages/vision/lib/glue/lib/widgets.dart
@@ -1,16 +1,17 @@
 import 'package:glue/module.dart';
 import 'package:vision/glue/lib/widgets/app.dart';
+import 'package:vision/glue/lib/widgets/canvas.dart';
 import 'package:vision/glue/lib/widgets/image.dart';
 import 'package:vision/glue/lib/widgets/placeholder.dart';
 import 'package:vision/glue/lib/widgets/screen.dart';
 import 'package:vision/persistent/assets.dart';
 
-/// State module providing reactive state management and UI components
 ModuleInfo widgetsModule(Assets assets) {
   return nativeModule('ffi.vision.widgets', [
     ('app', app),
     ('placeholder', placeholder),
     ('screen', screen),
     ('image', image(assets)),
+    ('canvas', canvas),
   ]);
 }
