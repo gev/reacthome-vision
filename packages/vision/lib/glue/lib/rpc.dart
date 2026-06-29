4bd587d295e090229d16504dbfbbd7c6ebe94710
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-29 22:44:19 +0300

    Fix rpc module description

--- packages/vision/lib/glue/lib/rpc.dart
+++ packages/vision/lib/glue/lib/rpc.dart
@@ -1,7 +1,7 @@
 import 'package:glue/module.dart';
 import 'package:vision/glue/lib/rpc/post.dart';
 
-/// Generic store module providing key-value storage
+/// RPC module providing remote procedure call
 ModuleInfo rpcModule(Sink<String> sink) {
   return nativeModule('ffi.vision.rpc', [('post', post(sink))]);
 }
