f68f78734fc463b13097be681742ea58e6cb112f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-01 02:13:25 +0300

    Rename paint to group in canvas module

--- packages/vision/lib/glue/lib/canvas.dart
+++ packages/vision/lib/glue/lib/canvas.dart
@@ -1,13 +1,13 @@
 import 'package:glue/module.dart';
 import 'package:vision/glue/lib/canvas/enums.dart';
+import 'package:vision/glue/lib/canvas/group.dart';
 import 'package:vision/glue/lib/canvas/line_to.dart';
 import 'package:vision/glue/lib/canvas/move_to.dart';
-import 'package:vision/glue/lib/canvas/paint.dart';
 import 'package:vision/glue/lib/canvas/paint_style.dart';
 import 'package:vision/glue/lib/canvas/path.dart';
 
 final ModuleInfo canvasModule = nativeModule('ffi.vision.canvas', [
-  ('paint', paint),
+  ('group', group),
   ('paint-style', paintStyle),
   ('path', path),
   ('move-to', moveTo),
--- packages/vision/lib/glue/lib/canvas/group.dart
+++ packages/vision/lib/glue/lib/canvas/group.dart
@@ -3,15 +3,15 @@ import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 import 'package:vision/glue/lib/canvas/handler.dart';
 
-final Ir paint = IrSpecial(paintImpl);
+final Ir group = IrSpecial(groupImpl);
 
-Eval<Ir> paintImpl(List<Ir> rawArgs) {
+Eval<Ir> groupImpl(List<Ir> rawArgs) {
   return sequenceAll(rawArgs.map(eval).toList()).bind((commands) {
-    return Eval.pure(IrNativeValue(Value(run(commands))));
+    return Eval.pure(IrNativeValue(Value(makeGroup(commands))));
   });
 }
 
-CanvasHandler run(List<Ir> commands) => (Canvas canvas) {
+CanvasHandler makeGroup(List<Ir> commands) => (Canvas canvas) {
   for (final command in commands) {
     if (command case IrNativeValue(
       value: Value(value: final CanvasHandler handle),
