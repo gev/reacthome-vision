f7a43af6f426fb7647d506a5aaf7c0f51c857744
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-06-30 19:33:33 +0300

    Add canvas widget implementation

--- packages/vision/lib/glue/lib/widgets/canvas.dart
+++ packages/vision/lib/glue/lib/widgets/canvas.dart
@@ -1,8 +1,17 @@
+import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:vision/glue/widgets/glue_canvas.dart';
 
-final Ir canvas = IrNativeFunc(canvasImpl);
-
-Eval<Ir> canvasImpl(Ir ir) {
-  return Eval.pure(IrVoid());
-}
+/// Creates a Canvas widget
+final Ir canvas = IrSpecial(
+  (List<Ir> ir) => getEnv().bind((env) {
+    switch (ir) {
+      case [Ir expreesion]:
+        final canvas = GlueCanvas(expression: expreesion, env: env);
+        return Eval.pure(IrNativeValue(Value(canvas)));
+      default:
+        return throwError(wrongArgumentType(['required canvas painter']));
+    }
+  }),
+);
