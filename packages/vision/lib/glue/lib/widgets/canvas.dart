8882e40cd926615520b13bce8f1c7af4f33fcc8e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-01 02:15:24 +0300

    Update canvas widget to support multiple commands
    Canvas widget evaluates commands and renders them as a group. Removed
    single-expression handling and added import for group utility.

--- packages/vision/lib/glue/lib/widgets/canvas.dart
+++ packages/vision/lib/glue/lib/widgets/canvas.dart
@@ -1,17 +1,15 @@
-import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:vision/glue/lib/canvas/group.dart';
 import 'package:vision/glue/widgets/glue_canvas.dart';
 
 /// Creates a Canvas widget
 final Ir canvas = IrSpecial(
-  (List<Ir> ir) => getEnv().bind((env) {
-    switch (ir) {
-      case [Ir expreesion]:
-        final canvas = GlueCanvas(expression: expreesion, env: env);
-        return Eval.pure(IrNativeValue(Value(canvas)));
-      default:
-        return throwError(wrongArgumentType(['required canvas painter']));
-    }
+  (List<Ir> rawArgs) => getEnv().bind((env) {
+    return sequenceAll(rawArgs.map(eval).toList()).bind((commands) {
+      final expression = IrNativeValue(Value(makeGroup(commands)));
+      final canvas = GlueCanvas(expression: expression, env: env);
+      return Eval.pure(IrNativeValue(Value(canvas)));
+    });
   }),
 );
