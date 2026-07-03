f1412085a804f84094cb69f86592d10b8d2130e6
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-03 22:07:19 +0300

    Update host address in makeLabApp

--- packages/apps/lab/lib/make_lab_app.dart
+++ packages/apps/lab/lib/make_lab_app.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:vision/app.dart';
 
 Future<Widget> makeLabApp() =>
-    makeApp(title: 'Lab', host: '127.0.0.1', port: 3005);
+    makeApp(title: 'Lab', host: '192.168.11.210', port: 3005);
