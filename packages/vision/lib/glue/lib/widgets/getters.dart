5cbf0f3f299812393178dbd78bff7b164e84d46b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-08 19:18:53 +0300

    Remove toIr helper function

--- packages/vision/lib/glue/lib/widgets/getters.dart
+++ packages/vision/lib/glue/lib/widgets/getters.dart
@@ -3,13 +3,3 @@ import 'package:glue/ir.dart';
 
 Map<String, Eval<Ir>> makeGetters(Map<String, dynamic> props) =>
     props.map((key, value) => MapEntry(key, Eval.pure(toIr(value))));
-
-Ir toIr(dynamic value) => switch (value) {
-  null => IrVoid(),
-  bool value => IrBool(value),
-  int value => IrInteger(value),
-  double value => IrFloat(value),
-  Iterable values => IrList(values.map(toIr).toList()),
-  IrNativeValue value => value,
-  _ => IrNativeValue(Value(value)),
-};
