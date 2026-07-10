fb2ade7d272711c2cfebb92038972b05e986967e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-10 03:02:37 +0300

    Refactor app.dart to extract route handling logic
    - Extracted route extraction logic into a separate `extractRoutes`
      function.
    - Simplified the `app` function to delegate to `appImpl`.
    - Removed unused imports (`vision/glue/route` and
      `vision/glue/widgets/glue_widget`).
    - Improved error messages for missing routes map and invalid route
      types.

--- packages/vision/lib/glue/lib/widgets/app.dart
+++ packages/vision/lib/glue/lib/widgets/app.dart
@@ -1,34 +1,37 @@
 import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
-import 'package:vision/glue/route.dart';
-import 'package:vision/glue/widgets/glue_widget.dart';
 
 /// Creates a Routes from an IrObject
-final Ir app = IrNativeFunc(
-  (Ir ir) => getEnv().bind((env) {
-    switch (ir) {
-      case IrObject(:final properties):
-        switch (properties['routes']) {
-          case IrObject(properties: final routes):
-            final builders = <String, RouteBuilder>{};
-            for (final route in routes.entries) {
-              builders[route.key] = (settings) {
-                final args = toIr(settings.arguments);
-                return (_) => GlueWidget(
-                  expression: IrList([route.value, args]),
-                  env: env,
-                );
-              };
-            }
-            return Eval.pure(IrNativeValue(Value((routes: builders))));
-          default:
-            return throwError(wrongArgumentType(['required routers map']));
-        }
+final Ir app = IrNativeFunc(appImpl);
+
+Eval<Ir> appImpl(Ir ir) {
+  switch (ir) {
+    case IrObject(:final properties):
+      switch (properties['routes']) {
+        case IrObject routes:
+          return extractRoutes(routes);
+        default:
+          return throwError(wrongArgumentType(['Required routers map']));
+      }
+    default:
+      return throwError(wrongArgumentType(['Required application propertis']));
+  }
+}
+
+Eval<Ir> extractRoutes(IrObject routes) {
+  final builders = <String, IrClosure>{};
+  for (final route in routes.properties.entries) {
+    switch (route.value) {
+      case IrClosure builder:
+        builders[route.key] = builder;
       default:
         return throwError(
-          wrongArgumentType(['required application propertis']),
+          wrongArgumentType([
+            'Route `${route.key}` should be a `lambda` accepted an `Object`',
+          ]),
         );
     }
-  }),
-);
+  }
+  return Eval.pure(IrNativeValue(Value((routes: builders))));
+}
