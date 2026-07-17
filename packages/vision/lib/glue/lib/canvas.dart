a2cd33b925ad5d07ca98d38119eb4b679a7b19c1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-17 17:31:14 +0300

    Add relative arc to point canvas glue binding

--- packages/vision/lib/glue/lib/canvas.dart
+++ packages/vision/lib/glue/lib/canvas.dart
@@ -5,6 +5,7 @@ import 'package:vision/glue/lib/canvas/line_to.dart';
 import 'package:vision/glue/lib/canvas/move_to.dart';
 import 'package:vision/glue/lib/canvas/paint_style.dart';
 import 'package:vision/glue/lib/canvas/path.dart';
+import 'package:vision/glue/lib/canvas/relativeArcToPoint.dart';
 
 final ModuleInfo canvasModule = nativeModule('ffi.vision.canvas', [
   ('group', group),
@@ -12,6 +13,7 @@ final ModuleInfo canvasModule = nativeModule('ffi.vision.canvas', [
   ('path', path),
   ('move-to', moveTo),
   ('line-to', lineTo),
+  ('relative-arc-to-point', relativeArcToPoint),
 
   ('blend-mode', blendMode),
   ('stroke-cap', strokeCap),
