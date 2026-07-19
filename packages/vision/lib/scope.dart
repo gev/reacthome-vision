d9ae7b772153d60032bedb3b4734a9691856dc08
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-19 15:05:13 +0300

    Remove unused session_monitor import from Scope

--- packages/vision/lib/scope.dart
+++ packages/vision/lib/scope.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:vision/glue/logger.dart';
 import 'package:vision/glue/stores/reactive_runtime.dart';
-import 'package:vision/session/session_monitor.dart';
 
 class Scope extends InheritedWidget {
   final Logger log;
