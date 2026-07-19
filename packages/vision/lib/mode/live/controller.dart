ef86c832e5148dffb478ed2f2f8c834bec205d16
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-19 17:33:59 +0300

    Move live mode controllers out of controllers dir

--- packages/vision/lib/mode/live/controller.dart
+++ packages/vision/lib/mode/live/controller.dart
@@ -3,7 +3,7 @@ import 'dart:developer';
 import 'dart:typed_data';
 
 import 'package:vision/glue/glue_controller.dart';
-import 'package:vision/mode/live/controllers/assets_controller.dart';
+import 'package:vision/mode/live/assets_controller.dart';
 
 sealed class Header {
   static const heartbeat = 0, glue = 1, file = 2;
