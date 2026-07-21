a295beafc76c40a7c12aa7602b4c76e295eafe37
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-22 00:38:23 +0300

    Add file watching to local orchestrator

--- packages/vision/lib/mode/local/local_orchestrator.dart
+++ packages/vision/lib/mode/local/local_orchestrator.dart
@@ -1,22 +1,36 @@
-import 'dart:io';
+import 'dart:async';
 
-import 'package:vision/glue/reactive_runtime.dart';
+import 'package:path/path.dart' as p;
 import 'package:vision/logger.dart';
 import 'package:vision/mode/local/glue/local_reactive_runtime.dart';
 import 'package:vision/mode/local/local_logger.dart';
+import 'package:watcher/watcher.dart';
 
 class LocalOrchestrator {
   final String _path;
 
   late final Logger log;
-  late final ReactiveRuntime reactiveRuntime;
+  late final LocalReactiveRuntime reactiveRuntime;
+  late final StreamSubscription<WatchEvent> _subscription;
 
   LocalOrchestrator({required this._path}) {
     log = LocalLogger();
     reactiveRuntime = LocalReactiveRuntime(path: _path, log: log);
+    final watcher = DirectoryWatcher(_path);
+    _subscription = watcher.events.listen(_watch);
+  }
+
+  void _watch(WatchEvent event) {
+    if (event.type == .MODIFY) {
+      final relativePath = p.relative(event.path, from: _path);
+      final pathWithoutExt = p.withoutExtension(relativePath);
+      final name = pathWithoutExt.replaceAll(p.separator, '.');
+      reactiveRuntime.loadModuleFromFile(name: name, path: event.path);
+    }
   }
 
   void dispose() {
+    _subscription.cancel();
     reactiveRuntime.dispose();
   }
 }
