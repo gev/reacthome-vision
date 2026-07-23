1320f71a8d83453e6ee1a1db28bf03340fac21bb
Nika <veronika.gazdovskaya@gmail.com>
2026-07-23 19:41:35 +0300

    Refactor relativeLineTo function for improved argument validation and error handling

--- packages/vision/lib/glue/lib/canvas/relative_line_to.dart
+++ packages/vision/lib/glue/lib/canvas/relative_line_to.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/widgets.dart';
+import 'package:glue/error.dart';
+import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
-import 'package:vision/glue/lib/canvas/handler.dart';
 
-final Ir relativeLineTo = handleXY(
-  (double x, double y) => (Path path) {
-    path.relativeLineTo(x, y);
-  },
-);
+final Ir relativeLineTo = IrNativeFunc((Ir arg) {
+  switch (arg) {
+    case IrObject(:final properties):
+      final dx = toDouble(properties['dx']);
+      if (dx == null) {
+        return throwError(wrongArgumentType(['dx should be double']));
+      }
+      final dy = toDouble(properties['dy']);
+      if (dy == null) {
+        return throwError(wrongArgumentType(['dy should be double']));
+      }
+      return Eval.pure(
+        IrNativeValue(
+          Value((Path path) {
+            path.relativeLineTo(dx, dy);
+          }),
+        ),
+      );
+    default:
+      return throwError(wrongArgumentType(['Object properties required']));
+  }
+});
