b2be14fbb3458f7f90e1e587a39a0b35457aaf3e
Nika <veronika.gazdovskaya@gmail.com>
2026-07-23 19:41:29 +0300

    Refactor moveTo function for improved argument validation and error handling

--- packages/vision/lib/glue/lib/canvas/move_to.dart
+++ packages/vision/lib/glue/lib/canvas/move_to.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/widgets.dart';
+import 'package:glue/error.dart';
+import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
-import 'package:vision/glue/lib/canvas/handler.dart';
 
-final Ir moveTo = handleXY(
-  (double x, double y) => (Path path) {
-    path.moveTo(x, y);
-  },
-);
+final Ir moveTo = IrNativeFunc((Ir arg) {
+  switch (arg) {
+    case IrObject(:final properties):
+      final x = toDouble(properties['x']);
+      if (x == null) {
+        return throwError(wrongArgumentType(['X should be double']));
+      }
+      final y = toDouble(properties['y']);
+      if (y == null) {
+        return throwError(wrongArgumentType(['Y should be double']));
+      }
+      return Eval.pure(
+        IrNativeValue(
+          Value((Path path) {
+            path.moveTo(x, y);
+          }),
+        ),
+      );
+    default:
+      return throwError(wrongArgumentType(['Object properties required']));
+  }
+});
