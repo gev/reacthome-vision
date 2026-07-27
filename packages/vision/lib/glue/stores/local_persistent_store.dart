1cec3c855b4bacd8a4d1d6986777fe1e0ac96cce
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-28 01:57:55 +0300

    Implement GluePut interface in LocalPersistentStore for enhanced data handling

--- packages/vision/lib/glue/stores/local_persistent_store.dart
+++ packages/vision/lib/glue/stores/local_persistent_store.dart
@@ -7,12 +7,14 @@ import 'package:vision/glue/stores/glue_reactive_cache.dart';
 import 'package:vision/glue/stores/glue_reactive_db.dart';
 import 'package:vision/glue/stores/glue_store.dart';
 import 'package:vision/logger.dart';
+import 'package:vision/store/revision.dart';
 
-class LocalPersistentStore implements GlueReactiveLookup {
+class LocalPersistentStore implements GlueReactiveLookup, GluePut {
   GlueDb? _local;
   GlueReactiveDb? _db;
   GlueReactiveCache? _cache;
   late final GlueReactiveLookup _lookup;
+  late final GluePut _put;
 
   LocalPersistentStore(Directory path, Logger log) {
     final local = makeGlueDb('local', path, log);
@@ -21,17 +23,24 @@ class LocalPersistentStore implements GlueReactiveLookup {
       final db = GlueReactiveDb(db: local, log: log);
       _db = db;
       _lookup = db;
+      _put = db;
     } else {
       log.warning('Fallback to temporary store');
       final cache = GlueReactiveCache();
       _cache = cache;
       _lookup = cache;
+      _put = cache;
     }
   }
 
   @override
   ValueNotifier<Ir> lookup(String key, Ir defaultValue) =>
       _lookup.lookup(key, defaultValue);
+
+  @override
+  void put(String key, Revision<Ir, int> value) {
+    _put.put(key, value);
+  }
 
   void dispose() {
     _cache?.dispose();
