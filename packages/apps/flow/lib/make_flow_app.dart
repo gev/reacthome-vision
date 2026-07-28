710920fcdfbf072925b22c5268f8161b2c361f73
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-28 18:00:47 +0300

    Update Flow app host IP address

--- packages/apps/flow/lib/make_flow_app.dart
+++ packages/apps/flow/lib/make_flow_app.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:vision/mode/live/live_app.dart';
 
 Future<Widget> makeFlowApp() =>
-    makeLiveApp(title: 'Flow', host: '127.0.0.1', port: 3005);
+    makeLiveApp(title: 'Flow', host: '192.168.11.210', port: 3005);
