c8ccdb49ef2d714400561bd443618cac4ed15a1a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-29 03:53:03 +0300

    Add locale widget to the widgets module and reorder theme entry

--- packages/vision/lib/glue/lib/widgets.dart
+++ packages/vision/lib/glue/lib/widgets.dart
@@ -4,6 +4,7 @@ import 'package:vision/glue/lib/widgets/activity.dart';
 import 'package:vision/glue/lib/widgets/app.dart';
 import 'package:vision/glue/lib/widgets/canvas.dart';
 import 'package:vision/glue/lib/widgets/closures.dart';
+import 'package:vision/glue/lib/widgets/locale.dart';
 import 'package:vision/glue/lib/widgets/media_query.dart';
 import 'package:vision/glue/lib/widgets/placeholder.dart';
 import 'package:vision/glue/lib/widgets/scaffold.dart';
@@ -30,7 +31,8 @@ ModuleInfo widgetsModule({required Ir image}) {
     ('right-gate', rightGate),
     ('shutter', shutter),
     ('app', app),
-    ('theme', theme),
+    ('locale', locale),
     ('media-query', mediaQuery),
+    ('theme', theme),
   ]);
 }
