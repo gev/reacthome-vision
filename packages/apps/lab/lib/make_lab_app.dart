c04d96c46c093a6ad7d12e0d0557e53d1f000fcd
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-15 01:24:52 +0300

    Refactor makeLabApp to use makeLocalApp instead of makeLocalScope

--- packages/apps/lab/lib/make_lab_app.dart
+++ packages/apps/lab/lib/make_lab_app.dart
@@ -1,13 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:vision/mode/local/local_scope.dart';
-import 'package:vision/widgets/vision_app.dart';
+import 'package:vision/mode/local/local_app.dart';
 
 Future<Widget> makeLabApp() {
   final path = Platform.environment['GLUE_PATH'] ?? "";
-  return makeLocalScope(
-    child: VisionApp(title: 'Lab'),
-    codePath: path,
-  );
+  return makeLocalApp(title: 'Lab', codePath: path);
 }
