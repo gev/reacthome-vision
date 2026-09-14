1b3154a7b648ff09101b5280de94c1b548667e9b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-15 00:18:23 +0300

    Rename widgetsModule to appletsModule

--- packages/vision/lib/glue/lib/applets.dart
+++ packages/vision/lib/glue/lib/applets.dart
@@ -2,7 +2,7 @@ import 'package:glue/module.dart';
 import 'package:vision/glue/lib/applets/live_applet.dart';
 import 'package:vision/glue/lib/applets/local_applet.dart';
 
-ModuleInfo widgetsModule = nativeModule('ffi.vision.applets', [
+ModuleInfo appletsModule = nativeModule('ffi.vision.applets', [
   ('live-applet', liveApplet),
   ('local-applet', localApplet),
 ]);
