50ca1e8b7f76e32bb9e5f96de3d2c394fdbbed6e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-15 01:22:10 +0300

    Add makeLiveApp function to create live app with specified parameters

---
+++ packages/vision/lib/mode/live/live_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:vision/mode/live/live_scope.dart';
+import 'package:vision/widgets/vision_app.dart';
+
+Future<Widget> makeLiveApp({
+  required String codePath,
+  required String host,
+  required int port,
+  required String title,
+}) async {
+  return makeLiveScope(
+    path: await getApplicationSupportDirectory(),
+    host: host,
+    port: port,
+    child: VisionApp(title: title),
+  );
+}
