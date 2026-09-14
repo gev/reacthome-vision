8fe04fc1bb7ffb1d87391afdb4ab4bf039b7d24c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-14 23:01:37 +0300

    Refactor makeFlowApp to use makeLocalScope and remove live scope configuration

--- packages/apps/flow/lib/make_flow_app.dart
+++ packages/apps/flow/lib/make_flow_app.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:vision/mode/live/live_scope.dart';
+import 'package:vision/mode/local/local_scope.dart';
 import 'package:vision/widgets/vision_app.dart';
 
 Future<Widget> makeFlowApp() {
-  return makeLiveScope(
-    host: '192.168.11.101',
-    port: 3005,
+  final path = Platform.environment['GLUE_PATH'] ?? "";
+  return makeLocalScope(
     child: VisionApp(title: 'Flow'),
+    codePath: path,
   );
+  // return makeLiveScope(
+  //   host: '192.168.11.101',
+  //   port: 3005,
+  //   child: VisionApp(title: 'Flow'),
+  // );
 }
