29eff5556515a07b5ece89b29ed529f7b7fa6386
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-15 01:27:15 +0300

    Remove commented-out makeLiveApp call from makeFlowApp function

--- packages/apps/flow/lib/make_flow_app.dart
+++ packages/apps/flow/lib/make_flow_app.dart
@@ -6,5 +6,4 @@ import 'package:vision/mode/local/local_app.dart';
 Future<Widget> makeFlowApp() {
   final codePath = Platform.environment['GLUE_PATH'] ?? "";
   return makeLocalApp(title: 'Flow', codePath: codePath);
-  // return makeLiveApp(title: 'Flow', host: '192.168.11.101', port: 3005);
 }
