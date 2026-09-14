6da4b3f73be976a54db248c98c65c33532f44072
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-15 01:02:37 +0300

    Refactor live and local applet implementations to accept Directory path and use VisionApplet

--- packages/vision/lib/glue/lib/applets/live_applet.dart
+++ packages/vision/lib/glue/lib/applets/live_applet.dart
@@ -1,24 +1,27 @@
+import 'dart:io';
+
 import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 import 'package:vision/mode/live/live_scope.dart';
-import 'package:vision/widgets/vision_app.dart';
+import 'package:vision/widgets/vision_applet.dart';
 
-final Ir liveApplet = IrNativeFunc(_liveAppletImpl);
+Ir liveApplet(Directory path) => IrNativeFunc(
+  (Ir props) => switch (props) {
+    IrObject(:final properties) => _createLiveApplet(path, properties.unlock),
+    _ => throwError(wrongArgumentType(['Properties `Object` required'])),
+  },
+);
 
-Eval<Ir> _liveAppletImpl(Ir props) => switch (props) {
-  IrObject(:final properties) => _createLiveApplet(properties.unlock),
-  _ => throwError(wrongArgumentType(['Properties `Object` required'])),
-};
-
-Eval<Ir> _createLiveApplet(Map<String, Ir> props) {
+Eval<Ir> _createLiveApplet(Directory path, Map<String, Ir> props) {
   final host = to<String>(props['host']) ?? '127.0.0.1';
   final port = to<int>(props['port']) ?? 3005;
   final title = to<String>(props['title']) ?? '';
   final widget = makeLiveScope(
+    path: path,
     host: host,
     port: port,
-    child: VisionApp(title: title),
+    child: VisionApplet(title: title),
   );
   return Eval.pure(IrNativeValue(Value(widget)));
 }
--- packages/vision/lib/glue/lib/applets/local_applet.dart
+++ packages/vision/lib/glue/lib/applets/local_applet.dart
@@ -1,22 +1,25 @@
+import 'dart:io';
+
 import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 import 'package:vision/mode/local/local_scope.dart';
-import 'package:vision/widgets/vision_app.dart';
+import 'package:vision/widgets/vision_applet.dart';
 
-final Ir localApplet = IrNativeFunc(_localAppletImpl);
+Ir localApplet(Directory path) => IrNativeFunc(
+  (Ir props) => switch (props) {
+    IrObject(:final properties) => _createLocalApplet(path, properties.unlock),
+    _ => throwError(wrongArgumentType(['Properties `Object` required'])),
+  },
+);
 
-Eval<Ir> _localAppletImpl(Ir props) => switch (props) {
-  IrObject(:final properties) => _createLocalApplet(properties.unlock),
-  _ => throwError(wrongArgumentType(['Properties `Object` required'])),
-};
-
-Eval<Ir> _createLocalApplet(Map<String, Ir> props) {
+Eval<Ir> _createLocalApplet(Directory path, Map<String, Ir> props) {
   final codePath = to<String>(props['code-path']) ?? '.';
   final title = to<String>(props['title']) ?? '';
   final widget = makeLocalScope(
+    path: path,
     codePath: codePath,
-    child: VisionApp(title: title),
+    child: VisionApplet(title: title),
   );
   return Eval.pure(IrNativeValue(Value(widget)));
 }
