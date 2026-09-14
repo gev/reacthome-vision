9fbbf396318f233b2df12bee99a18aaa85d057b2
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-15 01:03:00 +0300

    Refactor appletsModule to accept Directory path for live and local applets

--- packages/vision/lib/glue/lib/applets.dart
+++ packages/vision/lib/glue/lib/applets.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:glue/module.dart';
 import 'package:vision/glue/lib/applets/live_applet.dart';
 import 'package:vision/glue/lib/applets/local_applet.dart';
 
-ModuleInfo appletsModule = nativeModule('ffi.vision.applets', [
-  ('live-applet', liveApplet),
-  ('local-applet', localApplet),
-]);
+ModuleInfo appletsModule(Directory path) {
+  return nativeModule('ffi.vision.applets', [
+    ('live-applet', liveApplet(path)),
+    ('local-applet', localApplet(path)),
+  ]);
+}
