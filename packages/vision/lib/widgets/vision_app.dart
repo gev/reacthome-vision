b0d7b70469f71177c0bbcdefe95e947c1088b5c3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-14 22:12:12 +0300

    Refactor VisionApp to use mainEntryPoint and remove unused IrList

--- packages/vision/lib/widgets/vision_app.dart
+++ packages/vision/lib/widgets/vision_app.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:glue/ir.dart';
 import 'package:vision/glue/widgets/glue_app.dart';
 import 'package:vision/screens/splash_screen.dart';
+import 'package:vision/widgets/main_entry_point.dart';
 
 class VisionApp extends StatelessWidget {
   final String title;
 
-  VisionApp({required this.title, super.key});
-
-  // (
-  //   (import main)
-  //   main
-  // )
-  final _main = IrList([
-    IrList([IrSymbol('import'), IrSymbol('main')]),
-    IrSymbol('main'),
-  ]);
+  const VisionApp({required this.title, super.key});
 
   @override
   Widget build(BuildContext context) {
     return GlueApp(
       title: title,
-      app: _main,
+      app: mainEntryPoint,
       splash: SplashScreen(title: title, route: 'index'),
     );
   }
