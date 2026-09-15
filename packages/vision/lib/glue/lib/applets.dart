9e007098657654a5d7f669e186433fcf30ca16f4
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-16 02:26:54 +0300

    Refactor appletsModule to remove path parameter and simplify initialization

--- packages/vision/lib/glue/lib/applets.dart
+++ packages/vision/lib/glue/lib/applets.dart
@@ -1,12 +1,8 @@
-import 'dart:io';
-
 import 'package:glue/module.dart';
 import 'package:vision/glue/lib/applets/live_applet.dart';
 import 'package:vision/glue/lib/applets/local_applet.dart';
 
-ModuleInfo appletsModule(Directory path) {
-  return nativeModule('ffi.vision.applets', [
-    ('live-applet', liveApplet(path)),
-    ('local-applet', localApplet(path)),
-  ]);
-}
+ModuleInfo appletsModule = nativeModule('ffi.vision.applets', [
+  ('live-applet', liveApplet),
+  ('local-applet', localApplet),
+]);
