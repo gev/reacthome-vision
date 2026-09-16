aa3d36cd17e76225e8a8c050589699198962bacc
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-17 01:52:22 +0300

    Refactor navigator implementation to handle routes and improve error handling

--- packages/vision/lib/glue/lib/navigation/navigator.dart
+++ packages/vision/lib/glue/lib/navigation/navigator.dart
@@ -9,11 +9,20 @@ final Ir navigator = IrNativeFunc(navigatorImpl);
 
 Eval<Ir> navigatorImpl(Ir ir) {
   switch (ir) {
-    case IrObject app:
-      final props = WidgetProperties(app.properties.unlock);
-      return Eval.pure(
-        IrNativeValue(Value(GlueNavigator(key: props.key, app: app))),
-      );
+    case IrObject(:final properties):
+      final props = WidgetProperties(properties.unlock);
+      switch (props.get('routes')) {
+        case IrObject(properties: final routes):
+          return Eval.pure(
+            IrNativeValue(
+              Value(GlueNavigator(key: props.key, routes: routes.unlock)),
+            ),
+          );
+        default:
+          return throwError(
+            wrongArgumentType(['Object `routes` property required']),
+          );
+      }
     default:
       return throwError(
         wrongArgumentType(['Properties `Object` required for navigator']),
