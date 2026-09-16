19ac5dc71bfba0700711cf3e0ca08358b4b323bd
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-17 01:52:26 +0300

    Refactor GlueAppBaseState to improve initialization and streamline method names

--- packages/vision/lib/glue/widgets/glue_app_base_state.dart
+++ packages/vision/lib/glue/widgets/glue_app_base_state.dart
@@ -1,45 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:glue/context.dart';
-import 'package:glue/either.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 import 'package:vision/glue/app.dart';
 import 'package:vision/glue/extract.dart';
-import 'package:vision/scope.dart';
+import 'package:vision/glue/widgets/glue_navigator_base_state.dart';
 
-abstract class GlueAppBaseState<T extends StatefulWidget> extends State<T> {
+abstract class GlueAppBaseState<T extends StatefulWidget>
+    extends GlueNavigatorBaseState<T> {
   App cachedApp = defaultApp;
   Ir? lastEvaluatedExpression;
-  late final Scope scope;
-  bool initialized = false;
 
   Ir get app;
 
   @override
+  Routes get routes => cachedApp.routes;
+
+  bool _initialized = false;
+
+  @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    if (!initialized) {
-      initialized = true;
-      scope = Scope.of(context);
-      scope.reactiveRuntime.addListener(run);
+    if (!_initialized) {
+      _initialized = true;
+      scope.reactiveRuntime.addListener(_run);
     }
-    run();
+    _run();
   }
 
   @override
   void didUpdateWidget(T oldWidget) {
     super.didUpdateWidget(oldWidget);
-    runGuarded();
+    _runGuarded();
   }
 
-  void runGuarded() {
+  void _runGuarded() {
     if (lastEvaluatedExpression == app) {
       return;
     }
-    run();
+    _run();
   }
 
-  void run() {
+  void _run() {
     lastEvaluatedExpression = app;
 
     final evaluation = eval(app);
@@ -68,41 +70,9 @@ abstract class GlueAppBaseState<T extends StatefulWidget> extends State<T> {
     });
   }
 
-  Route<dynamic>? generateRoute(RouteSettings settings) {
-    final routeBuilder = cachedApp.routes[settings.name];
-    if (routeBuilder == null) {
-      scope.log.error('Route `${settings.name}` not found');
-      return null;
-    }
-
-    final evaluation = apply(routeBuilder, [toIr(settings.arguments)]);
-    final result = runEval(
-      evaluation,
-      scope.reactiveRuntime.runtime.copyWith(
-        context: putToContext<BuildContext>(
-          scope.reactiveRuntime.runtime.context,
-          context,
-        ),
-      ),
-    );
-
-    switch (result) {
-      case Left(value: final err):
-        scope.log.error('Route `${settings.name}` internal error $err');
-        return null;
-      case Right(:final value):
-        final route = to<Route<Ir>>(value.$1);
-        if (route == null) {
-          scope.log.error('Route `${settings.name}` not found');
-          return null;
-        }
-        return route;
-    }
-  }
-
   @override
   void dispose() {
-    scope.reactiveRuntime.removeListener(run);
+    scope.reactiveRuntime.removeListener(_run);
     super.dispose();
   }
 }
