175f1545e52c8e94d6bb5a49f2c67d9031f37ffd
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-17 04:21:14 +0300

    Refactor pushReplacement to use findTargetNavigator for improved route handling

--- packages/vision/lib/glue/lib/navigation/push_replacement.dart
+++ packages/vision/lib/glue/lib/navigation/push_replacement.dart
@@ -3,6 +3,7 @@ import 'package:glue/context.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 import 'package:vision/glue/app.dart';
+import 'package:vision/glue/widgets/glue_navigator_finder.dart';
 
 /// Replaces the current route with a new Route
 final Ir pushReplacement = IrNativeFunc((Ir ir) {
@@ -10,7 +11,7 @@ final Ir pushReplacement = IrNativeFunc((Ir ir) {
     final context = getFromContext<BuildContext>(runtime.context);
     if (context != null) {
       return evalRoute(ir).bind((entry) {
-        final navigator = Navigator.of(context);
+        final navigator = findTargetNavigator(context, entry.route);
         return routeResult(
           navigator.pushReplacementNamed(entry.route, arguments: entry.args),
         );
