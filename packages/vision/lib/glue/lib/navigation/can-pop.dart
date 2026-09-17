e62d911f160fa569a9895154e586ba43142063da
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-17 05:16:38 +0300

    Refactor canPop to use canPopNavigator for improved navigation stack checks

--- packages/vision/lib/glue/lib/navigation/can-pop.dart
+++ packages/vision/lib/glue/lib/navigation/can-pop.dart
@@ -2,13 +2,14 @@ import 'package:flutter/widgets.dart';
 import 'package:glue/context.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:vision/glue/widgets/glue_navigator_finder.dart';
 
 /// Checks if the navigation stack can pop
 final canPop = IrEvaluable(
   () => getRuntime().bind((runtime) {
     final context = getFromContext<BuildContext>(runtime.context);
     return Eval.pure(
-      IrBool(context != null ? Navigator.of(context).canPop() : false),
+      IrBool(context != null ? canPopNavigator(context) : false),
     );
   }),
 );
