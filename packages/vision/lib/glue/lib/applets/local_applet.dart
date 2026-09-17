f1c46a1b322ee7efab3fb7f19d6fa2dba9dd969d
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-18 02:35:21 +0300

    Refactor _createLocalApplet to accept WidgetProperties for improved type safety and clarity

--- packages/vision/lib/glue/lib/applets/local_applet.dart
+++ packages/vision/lib/glue/lib/applets/local_applet.dart
@@ -1,27 +1,28 @@
-import 'package:flutter/widgets.dart';
 import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:glue_flutter/glue_flutter.dart';
 import 'package:vision/scope_factory.dart';
 
 Ir localApplet = IrNativeFunc(
   (Ir props) => switch (props) {
-    IrObject(:final properties) => _createLocalApplet(properties.unlock),
+    IrObject(:final properties) => _createLocalApplet(
+      WidgetProperties(properties.unlock),
+    ),
     _ => throwError(wrongArgumentType(['Properties `Object` required'])),
   },
 );
 
-Eval<Ir> _createLocalApplet(Map<String, Ir> props) {
-  final id = toString(props['id']);
-  final key = toString(props['key']);
+Eval<Ir> _createLocalApplet(WidgetProperties props) {
+  final id = props.getString('id');
   if (id == null) {
     return throwError(wrongArgumentType(['Applet `id` required']));
   }
   final widget = ScopeFactory.makeLocalApplet(
     id: id,
-    key: key != null ? ValueKey(key) : null,
-    title: toString(props['title']) ?? '',
-    codePath: toString(props['code-path']) ?? '.',
+    key: props.key,
+    title: props.getString('title') ?? '',
+    codePath: props.getString('code-path') ?? '.',
   );
   return Eval.pure(IrNativeValue(Value(widget)));
 }
