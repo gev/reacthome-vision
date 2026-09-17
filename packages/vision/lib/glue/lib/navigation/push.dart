57c9eda605c6213d2dc6a796709abb71fa2eee04
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-17 04:20:27 +0300

    Refactor push function to use findTargetNavigator for improved route handling

--- packages/vision/lib/glue/lib/navigation/push.dart
+++ packages/vision/lib/glue/lib/navigation/push.dart
@@ -3,14 +3,14 @@ import 'package:glue/context.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 import 'package:vision/glue/app.dart';
+import 'package:vision/glue/widgets/glue_navigator_finder.dart';
 
-/// Pushes a Route onto the navigation stack
 final Ir push = IrNativeFunc((Ir ir) {
   return getRuntime().bind((runtime) {
     final context = getFromContext<BuildContext>(runtime.context);
     if (context != null) {
       return evalRoute(ir).bind((entry) {
-        final navigator = Navigator.of(context);
+        final navigator = findTargetNavigator(context, entry.route);
         return routeResult(
           navigator.pushNamed(entry.route, arguments: entry.args),
         );
