381cf5a800378c6b298c8d5e345b11ed4744a7d2
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-17 04:11:58 +0300

    Add navigatorKey to GlueApp for improved navigation management

--- packages/vision/lib/glue/widgets/glue_app.dart
+++ packages/vision/lib/glue/widgets/glue_app.dart
@@ -46,6 +46,7 @@ class _GlueAppState extends GlueAppBaseState<GlueApp>
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: widget.title,
+      navigatorKey: navigatorKey,
       key: ValueKey((_currentLocale, cachedApp)),
       themeMode: ThemeMode.system,
       theme: makeTheme(cachedApp, Brightness.light),
