b8857c56380ca1c775744aa9ef599d8bdf51901f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-18 02:35:14 +0300

    Refactor _createLiveApplet to use WidgetProperties for improved type safety and clarity

--- packages/vision/lib/glue/lib/applets/live_applet.dart
+++ packages/vision/lib/glue/lib/applets/live_applet.dart
@@ -1,28 +1,29 @@
-import 'package:flutter/widgets.dart';
 import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:glue_flutter/glue_flutter.dart';
 import 'package:vision/scope_factory.dart';
 
 Ir liveApplet = IrNativeFunc(
   (Ir props) => switch (props) {
-    IrObject(:final properties) => _createLiveApplet(properties.unlock),
+    IrObject(:final properties) => _createLiveApplet(
+      WidgetProperties(properties.unlock),
+    ),
     _ => throwError(wrongArgumentType(['Properties `Object` required'])),
   },
 );
 
-Eval<Ir> _createLiveApplet(Map<String, Ir> props) {
-  final id = toString(props['id']);
-  final key = toString(props['key']);
+Eval<Ir> _createLiveApplet(WidgetProperties props) {
+  final id = props.getString('id');
   if (id == null) {
     return throwError(wrongArgumentType(['Applet `id` required']));
   }
   final widget = ScopeFactory.makeLiveApplet(
     id: id,
-    key: key != null ? ValueKey(key) : null,
-    title: toString(props['title']) ?? '',
-    host: toString(props['host']) ?? '127.0.0.1',
-    port: toInt(props['port']) ?? 3005,
+    key: props.key,
+    title: props.getString('title') ?? '',
+    host: props.getString('host') ?? '127.0.0.1',
+    port: props.getInt('port') ?? 3005,
   );
   return Eval.pure(IrNativeValue(Value(widget)));
 }
