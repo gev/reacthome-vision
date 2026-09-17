cc814e0459d1d0fd745feae62e90452d0ab01bdb
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-17 05:16:46 +0300

    Refactor pop function to use findNavigatorToPop for improved navigation handling

--- packages/vision/lib/glue/lib/navigation/pop.dart
+++ packages/vision/lib/glue/lib/navigation/pop.dart
@@ -2,13 +2,14 @@ import 'package:flutter/widgets.dart';
 import 'package:glue/context.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:vision/glue/widgets/glue_navigator_finder.dart';
 
 /// Pops the current route, optionally passing a result
 final Ir pop = IrNativeFunc(
   (Ir resultIr) => getRuntime().bind((runtime) {
     final context = getFromContext<BuildContext>(runtime.context);
     if (context != null) {
-      Navigator.of(context).pop(resultIr);
+      findNavigatorToPop(context)?.pop(resultIr);
     }
     return Eval.pure(IrVoid());
   }),
