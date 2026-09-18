3a1a9ccfc448880ad72fe06b4854d63fcf0d2889
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-18 03:32:43 +0300

    Refactor canPop function to support optional target and root checks for improved navigation handling

--- packages/vision/lib/glue/lib/navigation/can_pop.dart
+++ packages/vision/lib/glue/lib/navigation/can_pop.dart
@@ -2,13 +2,23 @@ import 'package:flutter/widgets.dart';
 import 'package:glue/context.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:glue_flutter/glue_flutter.dart';
+import 'package:vision/glue/lib/navigation/find_navigator.dart';
 
-/// Checks if the navigation stack can pop
-final canPop = IrEvaluable(
-  () => getRuntime().bind((runtime) {
+/// Checks if the navigation stack can pop, supporting optional :target and root checks
+Ir canPop(bool rootNavigator) => IrNativeFunc((Ir ir) {
+  return getRuntime().bind((runtime) {
     final context = getFromContext<BuildContext>(runtime.context);
-    return Eval.pure(
-      IrBool(context != null ? Navigator.of(context).canPop() : false),
-    );
-  }),
-);
+    if (context != null) {
+      final targetNavigator = switch (ir) {
+        IrObject(:final properties) => extractFromGlobalKey(
+          properties['target'],
+        ),
+        _ => null,
+      };
+      final navigator = findNavigator(context, rootNavigator, targetNavigator);
+      return Eval.pure(IrBool(navigator.canPop()));
+    }
+    return Eval.pure(IrBool(false));
+  });
+});
