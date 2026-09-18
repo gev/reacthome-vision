200e69e178f1a8010bb4e43a4997e288a957e93b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-18 03:06:56 +0300

    Add target parameter to RouteEntry for enhanced navigation control

--- packages/vision/lib/glue/app.dart
+++ packages/vision/lib/glue/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
+import 'package:glue_flutter/glue_flutter.dart';
 
 class App {
   final Color? seedColor;
@@ -14,21 +15,29 @@ App defaultApp = App(routes: {});
 
 typedef Routes = Map<String, Ir>;
 
-typedef RouteEntry = ({String route, IrObject args});
+typedef RouteEntry = ({String route, NavigatorState? target, IrObject args});
 
 Eval<RouteEntry> evalRoute(Ir ir) {
   if (ir case IrObject(:final properties)) {
     Ir? route;
+    Ir? target;
     final args = <String, Ir>{};
     for (final entry in properties.entries) {
-      if (entry.key == 'route') {
-        route = entry.value;
-      } else {
-        args[entry.key] = entry.value;
+      switch (entry.key) {
+        case 'route':
+          route = entry.value;
+        case 'target':
+          target = entry.value;
+        default:
+          args[entry.key] = entry.value;
       }
     }
     if (route case IrSymbol(:final value)) {
-      return Eval.pure((route: value, args: IrObject(args)));
+      return Eval.pure((
+        route: value,
+        target: extractFromGlobalKey(target),
+        args: IrObject(args),
+      ));
     }
   }
   return throwError(wrongArgumentType(['Object with `route` required ']));
