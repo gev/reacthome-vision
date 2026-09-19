f5a3fc0a81d4c27ddd6b2ae2c4a9ea3e551a33e7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-19 14:42:36 +0300

    Remove commented key assignment in GlueApp for cleaner code

--- packages/vision/lib/glue/widgets/glue_app.dart
+++ packages/vision/lib/glue/widgets/glue_app.dart
@@ -46,7 +46,8 @@ class _GlueAppState extends GlueAppBaseState<GlueApp>
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: widget.title,
-      key: ValueKey((_currentLocale, cachedApp)),
+      // key: ValueKey((_currentLocale, cachedApp)),
+      key: ValueKey(cachedApp),
       themeMode: ThemeMode.system,
       theme: makeTheme(cachedApp, Brightness.light),
       darkTheme: makeTheme(cachedApp, Brightness.dark),
