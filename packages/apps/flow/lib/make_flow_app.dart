b7a52d7fe5f946735e23bd87ea0e162b71761980
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-21 02:17:38 +0300

    Add startDiscovery call in makeFlowApp to initiate network announcements

--- packages/apps/flow/lib/make_flow_app.dart
+++ packages/apps/flow/lib/make_flow_app.dart
@@ -1,9 +1,16 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:vision/discovery/discovery.dart';
 import 'package:vision/mode/local/local_app.dart';
 
 Future<Widget> makeFlowApp() {
+  startDiscovery(
+    onAnnounce: (message, address) {
+      print(address);
+      print(message);
+    },
+  );
   final codePath = Platform.environment['GLUE_PATH'] ?? "";
   return makeLocalApp(title: 'Flow', codePath: codePath);
 }
