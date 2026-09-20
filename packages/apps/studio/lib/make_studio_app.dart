b98126b7207692e27a7b11a8ad2dc303b9823caa
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-20 16:02:58 +0300

    Refactor i18n and related components to replace 'host' with 'url'

--- packages/apps/studio/lib/make_studio_app.dart
+++ packages/apps/studio/lib/make_studio_app.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:vision/mode/live/live_app.dart';
 
 Future<Widget> makeStudioApp() =>
-    makeLiveApp(title: 'Studio', uri: 'ws://127.0.0.1');
+    makeLiveApp(title: 'Studio', url: 'ws://127.0.0.1');
