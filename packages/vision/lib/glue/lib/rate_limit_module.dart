8faa7fdc48cdf3478d51a8a42f0e64ad586502ae
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-22 02:54:09 +0300

    Fix module identifier in rate limit module to include 'vision' namespace

--- packages/vision/lib/glue/lib/rate_limit_module.dart
+++ packages/vision/lib/glue/lib/rate_limit_module.dart
@@ -3,7 +3,7 @@ import 'package:vision/glue/lib/rate_limit/debounce.dart';
 import 'package:vision/glue/lib/rate_limit/throttle.dart';
 import 'package:vision/logger.dart';
 
-ModuleInfo rateLimitModule(Logger log) => nativeModule('ffi.rate-limit.rpc', [
-  ('debounce', debounce(log)),
-  ('throttle', throttle(log)),
-]);
+ModuleInfo rateLimitModule(Logger log) => nativeModule(
+  'ffi.vision.rate-limit',
+  [('debounce', debounce(log)), ('throttle', throttle(log))],
+);
