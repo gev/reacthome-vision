a9b88270f1121c93fe9a3c3746627f9acbeb49e0
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-22 21:54:52 +0300

    Enhance MediaPlayer with additional properties for customization

--- packages/vision/lib/glue/lib/media/media_player.dart
+++ packages/vision/lib/glue/lib/media/media_player.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:glue/error.dart';
 import 'package:glue/eval.dart';
 import 'package:glue/ir.dart';
 import 'package:glue_flutter/glue_flutter.dart';
+import 'package:media_kit_video/media_kit_video.dart';
 import 'package:vision/widgets/media/video_player.dart';
 
 Ir mediaPlayer = IrNativeFunc(
@@ -21,7 +23,38 @@ Eval<Ir> _createMediaPlayer(WidgetProperties props) {
     );
   }
 
-  final widget = MediaPlayer(key: props.key, url: url);
+  return getRuntime().map((runtime) {
+    final widget = MediaPlayer(
+      key: props.key,
+      url: url,
+      width: props.width,
+      height: props.height,
+      fit: props.getValue<BoxFit>('fit') ?? BoxFit.contain,
+      fill: props.getColor('fill') ?? Color(0xFF000000),
+      alignment: props.getValue<Alignment>('alignment') ?? Alignment.center,
+      aspectRatio: props.getDouble('aspect-ratio'),
+      filterQuality:
+          props.getValue<FilterQuality>('filter-quality') ?? FilterQuality.low,
+      // controls: props.getCallback<VideoState>('controls')?.call(runtime),
+      wakelock: props.getBool('wakelock') ?? true,
+      pauseUponEnteringBackgroundMode:
+          props.getValue<bool>('pause-upon-entering-background-mode') ?? true,
+      resumeUponEnteringForegroundMode:
+          props.getValue<bool>('resume-upon-entering-foreground-mode') ?? false,
+      subtitleViewConfiguration:
+          props.getValue<SubtitleViewConfiguration>(
+            'subtitle-view-configuration',
+          ) ??
+          const SubtitleViewConfiguration(),
+      focusNode: props.getValue<FocusNode>('focus-node'),
+      // onEnterFullscreen: props
+      //     .getVoidCallback('on-enter-fullscreen')
+      //     ?.call(runtime),
+      // onExitFullscreen: props
+      //     .getVoidCallback('on-exit-fullscreen')
+      //     ?.call(runtime),
+    );
 
-  return Eval.pure(IrNativeValue(Value(widget)));
+    return IrNativeValue(Value(widget));
+  });
 }
