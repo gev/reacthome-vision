d0aeadd562fb32f63e5a201841081ae55019d417
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-22 03:24:24 +0300

    Remove discovery functionality from makeFlowApp to streamline app initialization

--- packages/apps/flow/lib/make_flow_app.dart
+++ packages/apps/flow/lib/make_flow_app.dart
@@ -1,16 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:vision/discovery/discovery.dart';
 import 'package:vision/glue/runtime/local/local_app.dart';
 
 Future<Widget> makeFlowApp() {
-  startDiscovery(
-    onAnnounce: (message, address) {
-      print(address);
-      print(message);
-    },
-  );
   final codePath = Platform.environment['GLUE_PATH'] ?? "";
   return makeLocalApp(title: 'Flow', codePath: codePath);
 }
