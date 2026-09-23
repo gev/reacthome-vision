a06af119b35ae3c3e9ccf2bb9b4f221f6304c557
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-23 15:40:56 +0300

    Refactor makeFlowApp to use makeStaticLocalApp with package parameter instead of codePath

--- packages/apps/flow/lib/make_flow_app.dart
+++ packages/apps/flow/lib/make_flow_app.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:vision/glue/runtime/local/dynamic/dynamic_local_app.dart';
+import 'package:vision/glue/runtime/local/static/static_local_app.dart';
 
 Future<Widget> makeFlowApp() {
-  final codePath = Platform.environment['GLUE_PATH'] ?? "";
-  return makeDynamicLocalApp(title: 'Flow', codePath: codePath);
+  return makeStaticLocalApp(title: 'Flow', package: 'flow/glue');
 }
