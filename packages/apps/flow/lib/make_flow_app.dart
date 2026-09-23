d274bd0e669cd71cf942f44838d6190e1bfaca30
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-23 16:16:11 +0300

    Add 'makeDynamicLocalApp' example

--- packages/apps/flow/lib/make_flow_app.dart
+++ packages/apps/flow/lib/make_flow_app.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:vision/glue/runtime/local/static/static_local_app.dart';
 
 Future<Widget> makeFlowApp() {
+  // final codePath = Platform.environment['GLUE_PATH'] ?? "";
+  // return makeDynamicLocalApp(title: 'Flow', codePath: codePath);
   return makeStaticLocalApp(title: 'Flow', package: 'flow/glue');
 }
