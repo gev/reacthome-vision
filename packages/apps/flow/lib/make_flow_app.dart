38ff5fa1c53ffe3d9c72425a2e898d57cc740a68
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-09-23 16:17:56 +0300

    Refactor makeFlowApp to restore dynamic app functionality using codePath from environment variable

--- packages/apps/flow/lib/make_flow_app.dart
+++ packages/apps/flow/lib/make_flow_app.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:vision/glue/runtime/local/dynamic/dynamic_local_app.dart';
 import 'package:vision/glue/runtime/local/static/static_local_app.dart';
 
 Future<Widget> makeFlowApp() {
-  // final codePath = Platform.environment['GLUE_PATH'] ?? "";
-  // return makeDynamicLocalApp(title: 'Flow', codePath: codePath);
-  return makeStaticLocalApp(title: 'Flow', package: 'flow/glue');
+  final codePath = Platform.environment['GLUE_PATH'];
+  return codePath != null
+      ? makeDynamicLocalApp(title: 'Flow', codePath: codePath)
+      : makeStaticLocalApp(title: 'Flow', package: 'flow/glue');
 }
